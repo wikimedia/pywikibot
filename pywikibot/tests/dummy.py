fb5ef2e7bf3b3315ab6491ec46dd97114162b7c6
Russell Blau <russblau@users.mediawiki.org>
2007-12-26 15:57:59 +0000

    Add tests directory and dummy objects for use in developing and running unit tests.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/4759

---
+++ pywikibot/tests/dummy.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8  -*-
+"""Dummy objects for use in unit tests."""
+#
+# (C) Pywikipedia bot team, 2007
+#
+# Distributed under the terms of the MIT license.
+#
+__version__ = '$Id: $'
+
+# add in any other attributes or methods that are needed for testing
+
+class TestSite(object):
+    """Mimic a Site object."""
+    def __init__(self, hostname, protocol="http"):
+        self._hostname = hostname
+        self._protocol = protocol
+    def protocol(self):
+        return self._protocol
+    def hostname(self):
+        return self._hostname
+    def cookies(self, sysop=False):
+        if hasattr(self, "_cookies"):
+            return self._cookies
+        return u""
+
+
+class TestPage(object):
+    """Mimic a Page object."""
+    def __init__(self, site, title):
+        self._site = site
+        self._title = title
+
+    def site(self):
+        return self._site
+    def title(self):
+        return self._title
+    
