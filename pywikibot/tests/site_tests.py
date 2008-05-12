c81e9ccd7a79828dabca523682045818784bae35
Russell Blau <russblau@users.mediawiki.org>
2008-05-12 20:33:47 +0000

    add framework (incomplete) for testing site methods
    https://mediawiki.org/wiki/Special:Code/pywikipedia/5360

---
+++ pywikibot/tests/site_tests.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8  -*-
+"""
+Tests for the site module.
+"""
+#
+# (C) Pywikipedia bot team, 2008
+#
+# Distributed under the terms of the MIT license.
+#
+__version__ = '$Id: $'
+
+
+import unittest
+import pywikibot
+
+mysite = pywikibot.Site("en", "wikipedia")
+
+
+class TestSiteObject(unittest.TestCase):
+    """Test cases for Site methods."""
+    def testBaseMethods(self):
+        """Test cases for BaseSite methods"""
+        self.assertEqual(mysite.family(), pywikibot.site.Family("wikipedia"))
+        self.assertEqual(mysite.language(), "en")
+        self.assert_(isinstance(mysite.user(), (basestring, type(None))))
+        self.assertEqual(mysite.sitename(), "wikipedia:en")
+        self.assertEqual(mysite.normalize_namespace("project"), "Wikipedia")
+        self.assertEqual(mysite.redirect(), "REDIRECT")
+
+
+if __name__ == '__main__':
+    try:
+        try:
+            unittest.main()
+        except SystemExit:
+            pass
+    finally:
+        pywikibot.stopme()
