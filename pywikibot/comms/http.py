c4242b671e0e839f1a5faf16785c1777bd31c486
Russell Blau <russblau@users.mediawiki.org>
2008-10-27 20:22:38 +0000

    set user-agent header
    https://mediawiki.org/wiki/Special:Code/pywikipedia/6034

--- pywikibot/comms/http.py
+++ pywikibot/comms/http.py
@@ -90,6 +90,7 @@ def request(site, uri, *args, **kwargs):
     baseuri = "%s://%s/" % (site.protocol(), site.hostname())
     uri = urlparse.urljoin(baseuri, uri)
 
+    kwargs.setdefault("headers", {})['user-agent'] = useragent
     request = threadedhttp.HttpRequest(uri, *args, **kwargs)
     http_queue.put(request)
     request.lock.acquire()
