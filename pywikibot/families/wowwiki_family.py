bcb8a78026b6d3edda356d5138a03b5257551f0f
Russell Blau <russblau@users.mediawiki.org>
2009-10-27 17:19:38 +0000

    Update family files from trunk.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/7549

---
+++ pywikibot/families/wowwiki_family.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8  -*-
+
+__version__ = '$Id$'
+
+from pywikibot import family
+
+
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'wowwiki'
+
+        self.langs = {
+            'en': 'www.wowwiki.com',
+            'cs': 'cs.wow.wikia.com',
+            'da': 'da.wowwiki.com',
+            'de': 'de.wow.wikia.com',
+            'el': 'el.wow.wikia.com',
+            'es': 'es.wow.wikia.com',
+            'fa': 'fa.wow.wikia.com',
+            'fi': 'fi.wow.wikia.com',
+            'fr': 'fr.wow.wikia.com',
+            'he': 'he.wow.wikia.com',
+            'hu': 'hu.wow.wikia.com',
+            'is': 'is.wow.wikia.com',
+            'it': 'it.wow.wikia.com',
+            'ja': 'ja.wow.wikia.com',
+            'ko': 'ko.wow.wikia.com',
+            'lt': 'lt.wow.wikia.com',
+            'lv': 'lv.wow.wikia.com',
+            'nl': 'nl.wow.wikia.com',
+            'nn': 'nn.wow.wikia.com',
+            'no': 'no.wow.wikia.com',
+            'pl': 'pl.wow.wikia.com',
+            'pt': 'pt.wow.wikia.com',
+            'pt-br': 'pt-br.wow.wikia.com',
+            'ru': 'ru.wow.wikia.com',
+            'sk': 'sk.wow.wikia.com',
+            'zh': 'zh.wow.wikia.com',
+            'zh-tw': 'zh-tw.wow.wikia.com',
+        }
+
+        self.content_id = "article"
+
+        self.disambiguationTemplates['en'] = ['disambig', 'disambig/quest',
+                                              'disambig/quest2',
+                                              'disambig/achievement2']
+        self.disambcatname['en'] = "Disambiguations"
+
+    def scriptpath(self, code):
+        return ''
+
+    def version(self, code):
+        return '1.15.1'
