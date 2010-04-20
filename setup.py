993d81683a9570bd2bb1be66be42de4b10a3d2a4
Russell Blau <russblau@users.mediawiki.org>
2010-04-20 14:09:05 +0000

    Add installation instructions and update setup.py script
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8116

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
+import sys
+
 import ez_setup
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-setup(name='pywikibot',
-      version ='0.1.0dev',
+if sys.version_info[0] != 2:
+    raise RuntimeError("ERROR: Pywikipediabot only runs under Python 2")
+if sys.version_info[1] < 5:
+    raise RuntimeError("ERROR: Pywikipediabot only runs under Python 2.5 or higher")
+elif sys.version_info[1] == 5:
+    depend = ['simplejson', 'httplib2>=0.6.0']
+else:
+    depend = ['httplib2>=0.6.0']
+
+setup(name='Pywikipediabot',
+      version ='2.0alpha',
       description ='Python Wikipedia Bot Framework',
-      packages = find_packages(),
       license = 'MIT',
-      install_requites = ['simplejson', 'httplib2>=0.4.0']
+      packages = find_packages(),
+      install_requires = depend
      )
 
+# automatically launch generate_user_files.py
+
+import subprocess
+python = sys.executable
+python = python.replace("pythonw.exe", "python.exe") # for Windows
+ignore = subprocess.call([python, "generate_user_files.py"])
