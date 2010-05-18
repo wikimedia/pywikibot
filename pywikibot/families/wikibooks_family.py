6d2f99ec61fb8667445561ed40aa93e6409579ad
xqt <xqt@users.mediawiki.org>
2010-05-18 22:16:06 +0000

    update namespace alias for ro-wiki, update language_by_size, change interwiki order for be-x-old wikipedia to self.alphabetic (request #3002535)
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8195

--- pywikibot/families/wikibooks_family.py
+++ pywikibot/families/wikibooks_family.py
@@ -12,11 +12,11 @@ class Family(family.Family):
 
         self.languages_by_size = [
             'en', 'de', 'fr', 'ja', 'pt', 'hu', 'nl', 'es', 'pl', 'it', 'he',
-            'sq', 'fi', 'ru', 'vi', 'cs', 'hr', 'ca', 'zh', 'sv', 'da', 'tr',
+            'sq', 'fi', 'ru', 'vi', 'cs', 'hr', 'ca', 'sv', 'zh', 'da', 'tr',
             'th', 'mk', 'sr', 'fa', 'tl', 'ko', 'ar', 'id', 'gl', 'is', 'no',
             'ta', 'ka', 'lt', 'eo', 'bg', 'ro', 'sk', 'el', 'la', 'si', 'ia',
-            'ang', 'cv', 'sl', 'et', 'mr', 'uk', 'ur', 'ml', 'az', 'oc', 'ms',
-            'eu', 'hi', 'fy', 'ie', 'tg', 'bn', 'hy', 'af', 'pa', 'te', 'bs',
+            'ang', 'cv', 'sl', 'et', 'uk', 'mr', 'ur', 'ml', 'az', 'oc', 'ms',
+            'eu', 'hi', 'fy', 'ie', 'tg', 'bn', 'af', 'hy', 'pa', 'te', 'bs',
             'sa', 'ky', 'be', 'ast', 'tt', 'zh-min-nan', 'cy', 'mg', 'ne', 'ku',
             'co', 'sw', 'tk', 'su', 'als', 'vo', 'uz', 'na', 'mn', 'kk', 'nds',
             'zu', 'my',
