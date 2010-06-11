de9a6ae26e5cfbf4a7bf387ef0f3ffcc5271c2c9
xqt <xqt@users.mediawiki.org>
2010-06-11 09:31:44 +0000

    remove obsolete namespace dict.
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8276

--- pywikibot/families/wikia_family.py
+++ pywikibot/families/wikia_family.py
@@ -16,25 +16,6 @@ class Family(family.Family):
             u'wikia': None,
         }
 
-        self.namespaces[4] = {
-            '_default': [u'search', self.namespaces[4]['_default']],
-        }
-        self.namespaces[5] = {
-            '_default': [u'search talk', self.namespaces[5]['_default']],
-        }
-        self.namespaces[100] = {
-            '_default': u'Forum',
-        }
-        self.namespaces[101] = {
-            '_default': u'Forum talk',
-        }
-        self.namespaces[112] = {
-            '_default': u'Mini',
-        }
-        self.namespaces[113] = {
-            '_default': u'Mini talk',
-        }
-        
     def hostname(self, code):
         return u'www.wikia.com'
     
