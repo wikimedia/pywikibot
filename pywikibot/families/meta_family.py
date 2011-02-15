d1c33ad4b52a6f4ff3eba902251e26ee600aaa43
xqt <xqt@users.mediawiki.org>
2011-02-15 15:42:47 +0000

    MediaWiki version 1.17wmf1, update from r8955
    https://mediawiki.org/wiki/Special:Code/pywikipedia/8956

--- pywikibot/families/meta_family.py
+++ pywikibot/families/meta_family.py
@@ -16,12 +16,13 @@ class Family(family.Family):
         self.interwiki_forward = 'wikipedia'
         self.cross_allowed = ['meta',]
         self.cross_projects = [
-            'wikipedia', 'wiktionary', 'wikibooks', 'wikiquote', 'wikisource', 'wikinews', 'wikiversity',
-            'mediawiki', 'test', 'incubator', 'commons', 'species',
+            'wikipedia', 'wiktionary', 'wikibooks', 'wikiquote', 'wikisource',
+            'wikinews', 'wikiversity', 'mediawiki', 'test', 'incubator',
+            'commons', 'species',
         ]
 
     def version(self,code):
-        return '1.16wmf4'
+        return '1.17wmf1'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
