d0ce2b074ffd603c507069d8a5ab1189fad0ca56
xqt <xqt@users.mediawiki.org>
2011-03-12 23:21:01 +0000

    Update a version number from trunk r9016
    https://mediawiki.org/wiki/Special:Code/pywikipedia/9040

--- pywikibot/families/wikia_family.py
+++ pywikibot/families/wikia_family.py
@@ -20,7 +20,7 @@ class Family(family.Family):
         return u'www.wikia.com'
     
     def version(self, code):
-        return "1.15.1"
+        return "1.16.2"
 
     def scriptpath(self, code):
         return ''
