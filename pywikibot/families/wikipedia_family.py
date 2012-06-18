669e95d98c7d659a742cb7f042c6181688dec528
xqt <xqt@users.mediawiki.org>
2012-06-18 03:34:19 +0000

    nan-wiki is obsolete and replaced by zh-min-nan, update from trunk r10373
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10374

--- pywikibot/families/wikipedia_family.py
+++ pywikibot/families/wikipedia_family.py
@@ -400,6 +400,7 @@ class Family(family.Family):
             'minnan': 'zh-min-nan',
             'mo': 'ro', # http://meta.wikimedia.org/wiki/Proposals_for_closing_projects/Closure_of_Moldovan_Wikipedia
             'mus': None, # http://meta.wikimedia.org/wiki/Proposals_for_closing_projects/Closure_of_Muscogee_Wikipedia
+            'nan': 'zh-min-nan',
             'nb': 'no',
             'ng': None, #(not reachable) http://meta.wikimedia.org/wiki/Inactive_wikis
             'jp': 'ja',
