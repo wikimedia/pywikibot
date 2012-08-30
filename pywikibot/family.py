ad0de5396f67926a337b6c12ce55dee0d45f1902
xqt <xqt@users.mediawiki.org>
2012-08-30 19:58:53 +0000

    version 1.20wmf10
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10508

--- pywikibot/family.py
+++ pywikibot/family.py
@@ -822,7 +822,7 @@ class Family:
         """Return MediaWiki version number as a string."""
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
-        return '1.20wmf9'
+        return '1.20wmf10'
 
     def versionnumber(self, code):
         """Return an int identifying MediaWiki version.
