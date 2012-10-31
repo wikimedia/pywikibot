46351b49374ce796bac9e5c243b3f35834dab1ee
xqt <xqt@users.mediawiki.org>
2012-10-31 07:47:37 +0000

    shared_data_repository() introduced for wikidata clients
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10626

--- pywikibot/families/wikidata_family.py
+++ pywikibot/families/wikidata_family.py
@@ -4,12 +4,18 @@ __version__ = '$Id$'
 
 from pywikibot import family
 
-# The test wikidata family
+# The wikidata families
 class Family(family.Family):
     def __init__(self):
         family.Family.__init__(self)
         self.name = 'wikidata'
         self.langs = {
+            'wikidata': 'wikidata.org',
             'repo': 'wikidata-test-repo.wikimedia.de',
             'client': 'wikidata-test-client.wikimedia.de',
         }
+
+    def shared_image_repository(self, code):
+        # for here an now we just use the test repo
+        # for wikimedia families the method can return wikidata itself
+        return ('repo', 'wikidata')
