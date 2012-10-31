809960b0141e9c6021f4edb98c6aef3183833a00
xqt <xqt@users.mediawiki.org>
2012-10-31 16:57:22 +0000

    revert r8307 due to bug #3582204
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10633

--- pywikibot/textlib.py
+++ pywikibot/textlib.py
@@ -83,7 +83,7 @@ def replaceExcept(text, old, new, exceptions, caseInsensitive=False,
         # templates cascaded up to level 2, but no deeper. For arbitrary
         # depth, we'd need recursion which can't be done in Python's re.
         # After all, the language of correct parenthesis words is not regular.
-        'template':    re.compile(r'(?s){{(({{.*?}})|.)*}}'),
+        'template':    re.compile(r'(?s){{(({{.*?}})?.*?)*}}'),
         'hyperlink':   compileLinkR(),
         'gallery':     re.compile(r'(?is)<gallery.*?>.*?</gallery>'),
         # this matches internal wikilinks, but also interwiki, categories, and
