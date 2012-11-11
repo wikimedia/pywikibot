c053392a6b1cf3fc405df92329c189eaf9db719d
xqt <xqt@users.mediawiki.org>
2012-11-11 11:46:51 +0000

    some deprecate_arg arguments
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10697

--- pywikibot/compat/query.py
+++ pywikibot/compat/query.py
@@ -1,8 +1,12 @@
 import pywikibot
 from pywikibot.data import api
 from pywikibot import deprecated
+from pywikibot import deprecate_arg
 
 @deprecated("pywikibot.data.api.Request")
+@deprecate_arg("useAPI", None)
+@deprecate_arg("retryCount", None)
+@deprecate_arg("encodeTitle", None)
 def GetData(request, site=None, back_response=False):
     if site:
         request['site'] = site
