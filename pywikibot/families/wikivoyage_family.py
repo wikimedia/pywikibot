f36562439a1b64cfb169acbb7ec698981f6daffd
xqt <xqt@users.mediawiki.org>
2012-11-11 18:13:55 +0000

    wikivoyage family file, copy from trunk r10707
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10708

---
+++ pywikibot/families/wikivoyage_family.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '$Id$'
+
+# The new wikivoyage family that is hosted at wikimedia
+
+import family
+
+class Family(family.Family):
+    def __init__(self):
+        family.Family.__init__(self)
+        self.name = 'Wikivoyage'
+        self.langs = {
+            'de': u'de.wikivoyage.org',
+            'en': u'en.wikivoyage.org',
+            'fr': u'fr.wikivoyage.org',
+            'it': u'it.wikivoyage.org',
+            'nl': u'nl.wikivoyage.org',
+            'ru': u'ru.wikivoyage.org',
+            'sv': u'sv.wikivoyage.org',
+        }
+
+    def scriptpath(self, code):
+        return u'/w'
+
+    def shared_image_repository(self, code):
+        return ('commons', 'commons')
