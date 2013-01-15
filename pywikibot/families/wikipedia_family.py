68602ac673ffa0d3427dfcb7c56e991aa6d4c0b1
xqt <xqt@users.mediawiki.org>
2013-01-15 07:44:00 +0000

    preparing hu-wiki for wikidata; update from trunk r10923
    https://mediawiki.org/wiki/Special:Code/pywikipedia/10924

--- pywikibot/families/wikipedia_family.py
+++ pywikibot/families/wikipedia_family.py
@@ -580,5 +580,8 @@ class Family(family.Family):
     def shared_image_repository(self, code):
         return ('commons', 'commons')
 
-    def shared_data_repository(self, code):
-        return ('wikidata', 'wikidata')
+    def shared_data_repository(self, code, transcluded=False):
+        if not transcluded or code in ['hu']):
+            return ('wikidata', 'wikidata')            
+        else:
+            return (None, None)
