b2f2f47f4f4d195bd8b0f8b38d27e1bfd866db2e
xqt <xqt@users.mediawiki.org>
2013-03-01 15:53:42 +0000

    mw 1.21wmf10
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11143

--- pywikibot/family.py
+++ pywikibot/family.py
@@ -935,7 +935,7 @@ class WikimediaFamily(Family):
         """Return Wikimedia projects version number as a string."""
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
-        return '1.21wmf9'
+        return '1.21wmf10'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
