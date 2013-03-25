751a3211f81fcd848d89a7374588bbad3b64b1f6
legoktm <legoktm@users.mediawiki.org>
2013-03-25 07:29:54 +0000

    PEP8 fixes
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11259

--- pywikibot/exceptions.py
+++ pywikibot/exceptions.py
@@ -15,6 +15,7 @@ from pywikibot import config
 # TODO: These are copied from wikipedia.py; not certain that all of them
 # will be needed in the rewrite.
 
+
 class Error(Exception):
     """Wikipedia error"""
     def __init__(self, arg):
@@ -23,10 +24,13 @@ class Error(Exception):
             self.string = arg.encode(config.console_encoding, "xmlcharrefreplace")
         except (AttributeError, TypeError):
             self.string = arg.encode("ascii", "xmlcharrefreplace")
+
     def __str__(self):
         return self.string
+
     def __unicode__(self):
         return self.unicode
+
 
 class PageRelatedError(Error):
     """Abstract Exception, used when the Exception concerns a particular
@@ -35,6 +39,7 @@ class PageRelatedError(Error):
     # Override this in subclasses.
     # u"Oh noes! Page %s is too funky, we should not delete it ;("
     message = None
+
     def __init__(self, page):
         """
         @param page
@@ -48,23 +53,29 @@ class PageRelatedError(Error):
     def getPage(self):
         return self._page
 
+
 class NoUsername(Error):
     """Username is not in user-config.py"""
+
 
 class NoPage(PageRelatedError):
     """Page does not exist"""
     message = u"Page %s doesn't exist."
 
+
 class NoSuchSite(Error):
     """Site does not exist"""
+
 
 class IsRedirectPage(PageRelatedError):
     """Page is a redirect page"""
     message = u"Page %s is a redirect page."
 
+
 class IsNotRedirectPage(PageRelatedError):
     """Page is not a redirect page"""
     message = u"Page %s is not a redirect page."
+
 
 class CircularRedirect(Error):
     """Page is a circular redirect
@@ -75,21 +86,27 @@ class CircularRedirect(Error):
 
     """
 
+
 class InvalidTitle(Error):
     """Invalid page title"""
+
 
 class LockedPage(PageRelatedError):
     """Page is locked"""
     message = u"Page %s is locked."
 
+
 class SectionError(Error):
     """The section specified by # does not exist"""
+
 
 class PageNotSaved(Error):
     """Saving the page has failed"""
 
+
 class EditConflict(PageNotSaved):
     """There has been an edit conflict while uploading the page"""
+
 
 class SpamfilterError(PageNotSaved):
     """Saving the page has failed because the MediaWiki spam filter detected a blacklisted URL."""
@@ -98,11 +115,14 @@ class SpamfilterError(PageNotSaved):
         self.url = arg
         self.args = arg,
 
+
 class ServerError(Error):
     """Got unexpected server response"""
 
+
 class Server504Error(Error):
     """Server timed out with http 504 code"""
+
 
 class BadTitle(Error):
     """Server responded with BadTitle."""
@@ -110,17 +130,23 @@ class BadTitle(Error):
 # UserBlocked exceptions should in general not be caught. If the bot has
 # been blocked, the bot operator should address the reason for the block
 # before continuing.
+
+
 class UserBlocked(Error):
     """Your username or IP has been blocked"""
+
 
 class PageNotFound(Error):
     """Page not found in list"""
 
+
 class CaptchaError(Error):
     """Captcha is asked and config.solve_captcha == False."""
 
+
 class UploadWarning(Error):
     """Upload failed with a warning message (passed as the argument)."""
+
 
 class AutoblockUser(Error):
     """
@@ -128,5 +154,7 @@ class AutoblockUser(Error):
     an action is requested on a virtual autoblock user that's not available
     for him (i.e. roughly everything except unblock).
     """
+
+
 class UserActionRefuse(Error):
     pass
