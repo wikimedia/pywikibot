9cf605628c9458cd7269df3f448520d36de23e38
xqt <xqt@users.mediawiki.org>
2013-04-22 09:37:03 +0000

    set fallback to False for L10N, update from trunk r11428
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11429

--- scripts/clean_sandbox.py
+++ scripts/clean_sandbox.py
@@ -144,7 +144,9 @@ class SandboxBot(pywikibot.Bot):
         self.site = pywikibot.Site()
         self.site.login()
         if self.getOption('user'):
-            localSandboxTitle = pywikibot.translate(self.site, user_sandboxTemplate)
+            localSandboxTitle = pywikibot.translate(self.site,
+                                                    user_sandboxTemplate,
+                                                    fallback=False)
             localSandbox      = pywikibot.Page(self.site, localSandboxTitle)
             content.update(user_content)
             sandboxTitle[self.site.lang] = [item.title() \
@@ -162,7 +164,8 @@ class SandboxBot(pywikibot.Bot):
         while True:
             wait = False
             now = time.strftime("%d %b %Y %H:%M:%S (UTC)", time.gmtime())
-            localSandboxTitle = pywikibot.translate(self.site, sandboxTitle)
+            localSandboxTitle = pywikibot.translate(self.site, sandboxTitle,
+                                                    fallback=False)
             if type(localSandboxTitle) is list:
                 titles = localSandboxTitle
             else:
@@ -172,7 +175,8 @@ class SandboxBot(pywikibot.Bot):
                 pywikibot.output(u'Preparing to process sandbox page %s' % sandboxPage.title(asLink=True))
                 try:
                     text = sandboxPage.get()
-                    translatedContent = pywikibot.translate(self.site, content)
+                    translatedContent = pywikibot.translate(self.site, content,
+                                                            fallback=False)
                     translatedMsg = i18n.twtranslate(self.site,
                                                      'clean_sandbox-cleaned')
                     subst = 'subst:' in translatedContent
