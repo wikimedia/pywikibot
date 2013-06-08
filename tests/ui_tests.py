af31b81f803aedb2c7931390e6727cec6f43d725
Merlijn van Deen <valhallasw@users.mediawiki.org>
2013-06-08 21:23:39 +0000

    Improved reporting when ui_tests are run as part of setup.py test
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11627

--- tests/ui_tests.py
+++ tests/ui_tests.py
@@ -15,95 +15,92 @@ import cStringIO
 import StringIO
 import logging
 
-if __name__ != "__main__":
-    raise Exception('This test can only be run as single file due to heavy monkey patching')
+if __name__ == "__main__":
+    import sys
 
-import sys
+    oldstderr = sys.stderr
+    oldstdout = sys.stdout
+    oldstdin = sys.stdin
 
-oldstderr = sys.stderr
-oldstdout = sys.stdout
-oldstdin = sys.stdin
+    newstdout = cStringIO.StringIO()
+    newstderr = cStringIO.StringIO()
+    newstdin = StringIO.StringIO()
 
-newstdout = cStringIO.StringIO()
-newstderr = cStringIO.StringIO()
-newstdin = StringIO.StringIO()
+    def patch():
+        sys.stdout = newstdout
+        sys.stderr = newstderr
+        sys.stdin = newstdin
 
-def patch():
-    sys.stdout = newstdout
-    sys.stderr = newstderr
-    sys.stdin = newstdin
+    def unpatch():
+        sys.stdout = oldstdout
+        sys.stderr = oldstderr
+        sys.stdin = oldstdin
 
-def unpatch():
-    sys.stdout = oldstdout
-    sys.stderr = oldstderr
-    sys.stdin = oldstdin
-
-try:
-    patch()
-    import pywikibot
-finally:
-    unpatch()
-
-from pywikibot.bot import DEBUG, VERBOSE, INFO, STDOUT, INPUT, WARNING, ERROR, CRITICAL
-
-logger = logging.getLogger('pywiki')
-loggingcontext = {'caller_name': "ui_tests",
-                  'caller_file': "ui_tests",
-                  'caller_line': 0,
-                  'newline': "\n"}
-
-class TestTerminalUI(unittest.TestCase):
-    def setUp(self):
+    try:
         patch()
-        newstdout.truncate(0)
-        newstderr.truncate(0)
-        newstdin.truncate(0)
-
-    def tearDown(self):
+        import pywikibot
+    finally:
         unpatch()
 
-    def testOutputLevels_logging_debug(self):
-        logger.log(DEBUG, 'debug', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "")
+    from pywikibot.bot import DEBUG, VERBOSE, INFO, STDOUT, INPUT, WARNING, ERROR, CRITICAL
 
-    def testOutputLevels_logging_verbose(self):
-        logger.log(VERBOSE, 'verbose', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "")
+    logger = logging.getLogger('pywiki')
+    loggingcontext = {'caller_name': "ui_tests",
+                      'caller_file': "ui_tests",
+                      'caller_line': 0,
+                      'newline': "\n"}
 
-    def testOutputLevels_logging_info(self):
-        logger.log(INFO, 'info', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "info\n")
+    class TestTerminalUI(unittest.TestCase):
+        def setUp(self):
+            patch()
+            newstdout.truncate(0)
+            newstderr.truncate(0)
+            newstdin.truncate(0)
 
-    def testOutputLevels_logging_stdout(self):
-        logger.log(STDOUT, 'stdout', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "stdout\n")
-        self.assertEqual(newstderr.getvalue(), "")
+        def tearDown(self):
+            unpatch()
 
-    def testOutputLevels_logging_input(self):
-        logger.log(INPUT, 'input', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "input\n")
+        def testOutputLevels_logging_debug(self):
+            logger.log(DEBUG, 'debug', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "")
 
-    def testOutputLevels_logging_WARNING(self):
-        logger.log(WARNING, 'WARNING', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "WARNING: WARNING\n")
+        def testOutputLevels_logging_verbose(self):
+            logger.log(VERBOSE, 'verbose', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "")
 
-    def testOutputLevels_logging_ERROR(self):
-        logger.log(ERROR, 'ERROR', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "ERROR: ERROR\n")
+        def testOutputLevels_logging_info(self):
+            logger.log(INFO, 'info', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "info\n")
 
-    def testOutputLevels_logging_CRITICAL(self):
-        logger.log(CRITICAL, 'CRITICAL', extra=loggingcontext)
-        self.assertEqual(newstdout.getvalue(), "")
-        self.assertEqual(newstderr.getvalue(), "CRITICAL: CRITICAL\n")
+        def testOutputLevels_logging_stdout(self):
+            logger.log(STDOUT, 'stdout', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "stdout\n")
+            self.assertEqual(newstderr.getvalue(), "")
 
+        def testOutputLevels_logging_input(self):
+            logger.log(INPUT, 'input', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "input\n")
 
-if __name__ == '__main__':
+        def testOutputLevels_logging_WARNING(self):
+            logger.log(WARNING, 'WARNING', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "WARNING: WARNING\n")
+
+        def testOutputLevels_logging_ERROR(self):
+            logger.log(ERROR, 'ERROR', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "ERROR: ERROR\n")
+
+        def testOutputLevels_logging_CRITICAL(self):
+            logger.log(CRITICAL, 'CRITICAL', extra=loggingcontext)
+            self.assertEqual(newstdout.getvalue(), "")
+            self.assertEqual(newstderr.getvalue(), "CRITICAL: CRITICAL\n")
+
+    
     try:
         try:
             unittest.main()
@@ -112,3 +109,10 @@ if __name__ == '__main__':
     finally:
         unpatch()
         pywikibot.stopme()
+
+else:
+    class TestTerminalUI(unittest.TestCase):
+        @unittest.skip("Terminal UI tests can only be run by directly running tests/ui_tests.py")
+        def testCannotBeRun(self):
+            pass
+
