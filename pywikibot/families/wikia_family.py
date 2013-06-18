a7e3711cc18783a92116eccc7645ee9a98807637
legoktm <legoktm@users.mediawiki.org>
2013-06-18 07:30:56 +0000

    Bump version, update from trunk r11655
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11662

--- pywikibot/families/wikia_family.py
+++ pywikibot/families/wikia_family.py
@@ -20,7 +20,7 @@ class Family(family.Family):
         return u'www.wikia.com'
 
     def version(self, code):
-        return "1.16.5"
+        return "1.19.6"
 
     def scriptpath(self, code):
         return ''
