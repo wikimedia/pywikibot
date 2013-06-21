5257a87a39566a05e1e122ca86ba110280b9f917
xqt <xqt@users.mediawiki.org>
2013-06-21 07:51:56 +0000

    update from trunk r11677
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11678

--- pywikibot/textlib.py
+++ pywikibot/textlib.py
@@ -1060,6 +1060,7 @@ def glue_template_and_params(template_and_params):
 #----------------------------------
 
 def does_text_contain_section(pagetext, section):
+    section = re.sub('[ _]', '[ _]', section)
     """Determines whether the page text contains the given section title."""
-    m = re.search("=+[ ']*%s[ ']*=+" % re.escape(section), pagetext)
+    m = re.search("=+[ ']*%s[ ']*=+" % section, pagetext)
     return bool(m)
