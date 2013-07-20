33daff2b1b09bb4d91307cd477095ec1266aef28
xqt <xqt@users.mediawiki.org>
2013-07-20 10:52:33 +0000

    new scriptpath for wikidata test client
    https://mediawiki.org/wiki/Special:Code/pywikipedia/11770

--- pywikibot/families/wikidata_family.py
+++ pywikibot/families/wikidata_family.py
@@ -18,6 +18,11 @@ class Family(family.WikimediaFamily):
             'test': 'test.wikidata.org',
         }
 
+    def scriptpath(self, code):
+        if code == 'client':
+            return ''
+        return super(Family, self).scriptpath(code)
+
     def shared_data_repository(self, code, transcluded=False):
         """Always return a repository tupe. This enables testing whether
         the site opject is the repository itself, see Site.is_data_repository()
