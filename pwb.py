3e8885c3dd3fd50e57e3804d69cd2e29894f0780
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2013-07-29 13:16:16 +0200

    Updated pwb.py to better mirror direct script runs
    Before, execfile() ran the script in environment that pwb.py has, which
    included os and path imports, and several other variables.
    
    This commit uses run_python_file from Ned Batchelors' coverage.py [1],
    available under the BSD license [2]. See also his blog posts on the
    subject [3,4].
    
    This commit also adds a test that shells out to compare the locals()
    for runs through pwb.py as well as to a script directly.
    
    After this change, the effects of changeset 76484 can be seen using pwb.py
    
    [1] https://bitbucket.org/ned/coveragepy/src/b5abcee50dbe/coverage/execfile.py
    [2] https://bitbucket.org/ned/coveragepy/src/2c5fb3a8b81cc56d8ad57dd1bd83ef7740f0d65d/setup.py?at=default
    [3] http://nedbatchelder.com/blog/200904/running_a_python_file_as_main.html
    [4] http://nedbatchelder.com/blog/200905/running_a_python_file_as_main_take_2.html
    
    Change-Id: If9458fca50f07f08441dbb6e06f78bdbae2065de

--- pwb.py
+++ pwb.py
@@ -12,8 +12,50 @@ __version__ = '$Id$'
 # Distributed under the terms of the MIT license.
 #
 
-import sys
+
+# The following snippet was developed by Ned Batchelder (and others)
+# for coverage.py [1], and is available under the BSD license (see [2])
+# [1] https://bitbucket.org/ned/coveragepy/src/b5abcee50dbe/coverage/execfile.py
+# [2] https://bitbucket.org/ned/coveragepy/src/2c5fb3a8b81cc56d8ad57dd1bd83ef7740f0d65d/setup.py?at=default#cl-31
+
+import imp
 import os
+import sys
+
+
+def run_python_file(filename, args):
+    """Run a python file as if it were the main program on the command line.
+
+    `filename` is the path to the file to execute, it need not be a .py file.
+    `args` is the argument array to present as sys.argv, including the first
+    element representing the file being executed.
+
+    """
+    # Create a module to serve as __main__
+    old_main_mod = sys.modules['__main__']
+    main_mod = imp.new_module('__main__')
+    sys.modules['__main__'] = main_mod
+    main_mod.__file__ = filename
+    main_mod.__builtins__ = sys.modules['__builtin__']
+
+    # Set sys.argv and the first path element properly.
+    old_argv = sys.argv
+    old_path0 = sys.path[0]
+    sys.argv = args
+    sys.path[0] = os.path.dirname(filename)
+
+    try:
+        source = open(filename).read()
+        exec compile(source, filename, "exec") in main_mod.__dict__
+    finally:
+        # Restore the old __main__
+        sys.modules['__main__'] = old_main_mod
+
+        # Restore the old argv and path
+        sys.argv = old_argv
+        sys.path[0] = old_path0
+
+#### end of snippet
 
 if sys.version_info[0] != 2:
     raise RuntimeError("ERROR: Pywikipediabot only runs under Python 2")
@@ -33,21 +75,20 @@ if "PYWIKIBOT2_DIR" not in os.environ:
     os.environ["PYWIKIBOT2_DIR"] = os.path.split(__file__)[0]
 
 if not os.path.exists(os.path.join(os.environ["PYWIKIBOT2_DIR"], "user-config.py")):
-    execfile('generate_user_files.py')
+    run_python_file('generate_user_files.py', ['generate_user_files.py'])
 
-sys.argv.pop(0)
-if len(sys.argv) > 0:
-    if not os.path.exists(sys.argv[0]):
-        testpath = os.path.join(os.path.split(__file__)[0], 'scripts', sys.argv[0])
+if len(sys.argv) > 1:
+    fn = sys.argv[1]
+    args = sys.argv[1:]
+
+    if not os.path.exists(fn):
+        testpath = os.path.join(os.path.split(__file__)[0], 'scripts', fn)
         if os.path.exists(testpath):
-            sys.argv[0] = testpath
+            fn = testpath
         else:
             testpath = testpath + '.py'
             if os.path.exists(testpath):
-                sys.argv[0] = testpath
+                fn = testpath
             else:
-                raise Exception("%s not found!" % sys.argv[0])
-    sys.path.append(os.path.split(sys.argv[0])[0])
-    execfile(sys.argv[0])
-else:
-    sys.argv.append('')
+                raise Exception("%s not found!" % fn)
+    run_python_file(fn, args)
