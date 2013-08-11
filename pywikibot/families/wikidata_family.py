a93ad6ce9a264a82717a37230e48ff00d9c642fc
Kunal Mehta <legoktm@gmail.com>
2013-08-11 10:33:55 +0800

    Add moon (Q405) to the list of globes
    Change-Id: I2dd9f87fcb1d748bff94328575f8439dc36035e3

--- pywikibot/families/wikidata_family.py
+++ pywikibot/families/wikidata_family.py
@@ -40,4 +40,5 @@ class Family(family.WikimediaFamily):
 
     def globes(self, code):
         """Supported globes for Coordinate datatype"""
-        return {'earth': 'http://www.wikidata.org/entity/Q2'}
+        return {'earth': 'http://www.wikidata.org/entity/Q2',
+                'moon': 'http://www.wikidata.org/entity/Q405'}
