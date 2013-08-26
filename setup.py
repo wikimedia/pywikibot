5f3fcec2791960d283e5941381c55d1dcca68d99
Kunal Mehta <legoktm@gmail.com>
2013-08-26 00:36:33 -0700

    Revamp setup.py, bump version to 2.0b1
    core is no longer an alpha level project, beta is more accurate.
    Added various classifiers for eventual pypi classification
    
    Users can now access the version with pywikibot.__version__, which
    is standard Python convention. For version.py, this is added
    as the "release version".
    
    Change-Id: I1a58a4eceb77215d4aadb16e0708da03013e7668

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8  -*-
-"""installer script for pywikibot 2.0 framework"""
-#
-# (C) Pywikipedia team, 2009-2012
-#
+"""
+Installer script for Pywikibot 2.0 framework
+
+(C) Pywikipedia team, 2009-2013
+
+Distributed under the terms of the MIT license.
+
+"""
 __version__ = '$Id$'
-#
-# Distributed under the terms of the MIT license.
-#
+
 import sys
 
 from distribute_setup import use_setuptools
@@ -16,19 +18,29 @@ from setuptools import setup, find_packages
 
 if sys.version_info[0] != 2:
     raise RuntimeError("ERROR: Pywikipediabot only runs under Python 2")
-if sys.version_info[1] < 6:
+elif sys.version_info[1] < 6:
     raise RuntimeError("ERROR: Pywikipediabot only runs under Python 2.6 or higher")
-else:
-    depend = ['httplib2>=0.6.0']
 
-setup(name='Pywikipediabot',
-      version='2.0alpha',
-      description='Python Wikipedia Bot Framework',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=depend,
-      test_suite="tests",
-      )
+setup(
+    name='Pywikipediabot',
+    version='2.0b1',
+    description='Python Wikipedia Bot Framework',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=[
+        'httplib2>=0.6.0'
+    ],
+    test_suite="tests",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta'
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
 
 # automatically launch generate_user_files.py
 
