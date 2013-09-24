9b4619450638e3a5b49a91dd1f60274d967eaa32
xqt <info@gno.de>
2013-09-24 09:45:15 +0200

    update mw to 1.22wmf17
    Change-Id: I625354fb5868016e7fd5afc5d69bf712ddab9b78

--- pywikibot/family.py
+++ pywikibot/family.py
@@ -936,7 +936,7 @@ class WikimediaFamily(Family):
         """Return Wikimedia projects version number as a string."""
         # Don't use this, use versionnumber() instead. This only exists
         # to not break family files.
-        return '1.22wmf15'
+        return '1.22wmf17'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
