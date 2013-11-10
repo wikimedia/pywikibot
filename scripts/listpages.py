1ac6ce73ac20417e502010510aee3d02f9f12ae8
Pyfisch <pyfisch@googlemail.com>
2013-11-10 13:43:48 +0100

    Improvements to listpages.py
    1. Structure of listpages.py now like in other scripts.
    2. Removed uneeded except part which caused bad output.
    3. Fixed typing error. parameterd -> parameters
    
    Change-Id: Iacc0e929482b4baac12a078bdc9faf813f773444

--- scripts/listpages.py
+++ scripts/listpages.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8  -*-
 """
-Print a list of pages, as defined by page generator parameterd
+Print a list of pages, as defined by page generator parameters
 
 These parameters are supported to specify which pages titles to print:
 
@@ -20,23 +20,21 @@ docuReplacements = {'&params;': parameterHelp}
 
 
 def main(*args):
-    try:
-        gen = None
-        genFactory = GeneratorFactory()
-        for arg in pywikibot.handleArgs(*args):
-            genFactory.handleArg(arg)
-        gen = genFactory.getCombinedGenerator()
-        if gen:
-            i = 0
-            for page in gen:
-                i += 1
-                pywikibot.stdout("%4d: %s" % (i, page.title()))
-        else:
-            pywikibot.showHelp()
-    except Exception:
-        pywikibot.error("Fatal error", exc_info=True)
-    finally:
-        pywikibot.stopme()
+    gen = None
+    genFactory = GeneratorFactory()
+    for arg in pywikibot.handleArgs(*args):
+        genFactory.handleArg(arg)
+    gen = genFactory.getCombinedGenerator()
+    if gen:
+        i = 0
+        for page in gen:
+            i += 1
+            pywikibot.stdout("%4d: %s" % (i, page.title()))
+    else:
+        pywikibot.showHelp()
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
