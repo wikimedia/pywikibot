ef2b601c684e059eb678a6bd65dea030f7627669
xqt <info@gno.de>
2014-01-21 06:37:48 +0100

    remove unused local list, self.alphabetic from family.py does the job
    Change-Id: Ia2bfa2fdaaab49efe8827f623b9b23aad59603ff

--- pywikibot/families/battlestarwiki_family.py
+++ pywikibot/families/battlestarwiki_family.py
@@ -16,13 +16,6 @@ class Family(family.Family):
         for lang in self.languages_by_size:
             self.langs[lang] = '%s.battlestarwiki.org' % lang
 
-        # A few selected big languages for things that we do not want to loop over
-        # all languages. This is only needed by the titletranslate.py module, so
-        # if you carefully avoid the options, you could get away without these
-        # for another wiki family.
-
-        alphabetic = ['de', 'en', 'es', 'fr', 'tr', 'zh']
-
     def hostname(self, code):
         return '%s.battlestarwiki.org' % code
 
