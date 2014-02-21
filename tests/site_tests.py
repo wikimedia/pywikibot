7814c60217cc402774eae7162d8437523b15c6d3
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2014-02-21 22:56:09 +0100

    Add must_be unit tests
    Change-Id: I4ee61c6e550bb08c3e0f367e4cecafff81c9ad1c

--- tests/site_tests.py
+++ tests/site_tests.py
@@ -11,6 +11,7 @@ __version__ = '$Id$'
 
 
 import pywikibot
+from pywikibot.site import must_be
 from tests import patch_request, unpatch_request
 from utils import PywikibotTestCase, unittest
 
@@ -1105,6 +1106,57 @@ class TestSiteLoadRevisions(PywikibotTestCase):
 
         # TODO test other optional arguments
 
+
+class TestMustBe(PywikibotTestCase):
+    """Test cases for the must_be decorator."""
+
+    # Implemented without setUpClass(cls) and global variables as objects
+    # were not completely disposed and recreated but retained 'memory'
+    def setUp(self):
+        self._logged_in_as = None
+
+    def login(self, sysop):
+        # mock call
+        self._logged_in_as = 'sysop' if sysop else 'user'
+
+    def testMockInTest(self):
+        self.assertEqual(self._logged_in_as, None)
+        self.login(True)
+        self.assertEqual(self._logged_in_as, 'sysop')
+
+    testMockInTestReset = testMockInTest
+
+    @must_be('sysop')
+    def call_this_sysop_req_function(self, *args, **kwargs):
+        return args, kwargs
+
+    @must_be('user')
+    def call_this_user_req_function(self, *args, **kwargs):
+        return args, kwargs
+
+    def testMustBeSysop(self):
+        args = (1, 2, 'a', 'b')
+        kwargs = {'i': 'j', 'k': 'l'}
+        retval = self.call_this_sysop_req_function(*args, **kwargs)
+        self.assertEqual(retval[0], args)
+        self.assertEqual(retval[1], kwargs)
+        self.assertEqual(self._logged_in_as, 'sysop')
+
+    def testMustBeUser(self):
+        args = (1, 2, 'a', 'b')
+        kwargs = {'i': 'j', 'k': 'l'}
+        retval = self.call_this_user_req_function(*args, **kwargs)
+        self.assertEqual(retval[0], args)
+        self.assertEqual(retval[1], kwargs)
+        self.assertEqual(self._logged_in_as, 'user')
+
+    def testOverrideUserType(self):
+        args = (1, 2, 'a', 'b')
+        kwargs = {'i': 'j', 'k': 'l'}
+        retval = self.call_this_user_req_function(*args, as_group='sysop', **kwargs)
+        self.assertEqual(retval[0], args)
+        self.assertEqual(retval[1], kwargs)
+        self.assertEqual(self._logged_in_as, 'sysop')
 
 if __name__ == '__main__':
     try:
