3d64bdf05f7f5f0bb20c77f85753e1343eea331b
xqt <info@gno.de>
2014-03-23 14:31:20 +0100

    Remove commented out stuff, remove obsolete imports
    Import version.getversion ony instead of '*' import
    
    Change-Id: Iac9cd0215f368453642ce1733d143f1706b9c568

--- scripts/version.py
+++ scripts/version.py
@@ -3,7 +3,7 @@
 """ Script to determine the pywikipedia version (tag, revision and date) """
 #
 # (C) Merlijn 'valhallasw' van Deen, 2007-2008
-# (C) xqt, 2010-2012
+# (C) xqt, 2010-2014
 # (C) Pywikibot team, 2007-2013
 #
 # Distributed under the terms of the MIT license.
@@ -13,16 +13,12 @@ __version__ = '$Id$'
 
 import sys
 import pywikibot
-from pywikibot.version import *
-from pywikibot import config2 as config
+from pywikibot.version import getversion
 
 if __name__ == '__main__':
     pywikibot.output('Pywikibot: %s' % getversion())
     pywikibot.output('Release version: %s' % pywikibot.__release__)
     pywikibot.output('Python: %s' % sys.version)
-    #pywikibot.output('config-settings:')
-    #pywikibot.output('site_interface =', config.site_interface)
-    #pywikibot.output('API_config_expiry =', config.API_config_expiry)
     if not __import__('unicodedata').normalize('NFC', u'\u092e\u093e\u0930\u094d\u0915 \u091c\u093c\u0941\u0915\u0947\u0930\u092c\u0930\u094d\u0917') == u'\u092e\u093e\u0930\u094d\u0915 \u091c\u093c\u0941\u0915\u0947\u0930\u092c\u0930\u094d\u0917':
         pywikibot.output(u'unicode test: triggers problem #3081100')
     else:
