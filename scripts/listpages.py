edc54528361dca6382cf184fd6f36d09c1d4351a
xqt <info@gno.de>
2014-04-18 07:24:13 +0200

    Revert "Add custom formatting option to listpages"
    (page.title is a method, not an attribute with basestring representation like page.site for example)
    
    This reverts commit 53e64b500a80c0a8acca7a594286b7a0a00d5494.
    
    Change-Id: I2405e7b3de4f8ffbb7c12070409f35c1105befca

--- scripts/listpages.py
+++ scripts/listpages.py
@@ -4,12 +4,6 @@ Print a list of pages, as defined by page generator parameters
 
 These parameters are supported to specify which pages titles to print:
 
--format  Defines the output format.
-         Default: "{num:4d} {page.title}" (  10 PageTitle)
-
-         Other suggestions:
-         "# {page}" (# [[PageTitle]])
-
 &params;
 """
 #
@@ -28,16 +22,15 @@ docuReplacements = {'&params;': parameterHelp}
 
 def main(*args):
     gen = None
-    fmt = "{num:4d} {page.title}"
     genFactory = GeneratorFactory()
     for arg in pywikibot.handleArgs(*args):
-        if arg.startswith("-format:"):
-            fmt = arg[len("-format:"):]
         genFactory.handleArg(arg)
     gen = genFactory.getCombinedGenerator()
     if gen:
-        for i, page in enumerate(gen):
-            pywikibot.stdout(fmt.format(num=i, page=page))
+        i = 0
+        for page in gen:
+            i += 1
+            pywikibot.stdout("%4d: %s" % (i, page.title()))
     else:
         pywikibot.showHelp()
 
