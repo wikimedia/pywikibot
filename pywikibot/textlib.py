1da175216eb0df70f0eaa45af088907d6e51697b
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2014-04-27 23:02:42 +0200

    Make tests/api_tests.py run on py3k
    Syntactic/import changes:
    comms/http.py syntax/import
    data/wikidataquery.py import
    pywikibot/textlib.py syntax
    
    
    More elaborate changes:
    comms/threadedhttp.py: import, plus adapt DummyResponse/DummyRequest
                           to have Python3 urllib's required parameters
    pywikibot/throttle.py: close throttle file after use
    userinterfaces/terminal_interface_unix.py: only encode when target stream
                           is a bytes stream (i.e. doesn't have encoding
                           property)
    
    tests/api_tests.py: cleanup to more specific assertions.
    
    Change-Id: Ib639e7dc70ffc5f192aefacf64e2770c80629c8a

--- pywikibot/textlib.py
+++ pywikibot/textlib.py
@@ -1180,7 +1180,7 @@ class TimeStripper(object):
         timeR = r'(?P<time>(?P<hour>[0-2]\d)[:\.h](?P<minute>[0-5]\d))'
         timeznR = r'\((?P<tzinfo>[A-Z]+)\)'
         yearR = r'(?P<year>(19|20)\d\d)'
-        monthR = ur'(?P<month>(%s))' % (u'|'.join(self.origNames2monthNum))
+        monthR = r'(?P<month>(%s))' % (u'|'.join(self.origNames2monthNum))
         dayR = r'(?P<day>(3[01]|[12]\d|0?[1-9]))'
 
         self.ptimeR = re.compile(timeR)
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -19,9 +19,9 @@ class TestApiFunctions(unittest.TestCase):
     def testObjectCreation(self):
         """Test that api.Request() creates an object with desired attributes"""
         req = api.Request(site=mysite, action="test", foo="", bar="test")
-        self.assert_(req)
+        self.assertTrue(req)
         self.assertEqual(req.site, mysite)
-        self.assert_("foo" in req.params)
+        self.assertIn("foo", req.params)
         self.assertEqual(req["bar"], "test")
         # test item assignment
         req["one"] = "1"
@@ -29,8 +29,9 @@ class TestApiFunctions(unittest.TestCase):
         # test compliance with dict interface
         # req.keys() should contain "action", "foo", "bar", "one"
         self.assertEqual(len(req.keys()), 4)
-        self.assert_("test" in req.values())
-        self.assert_(all(len(item) == 2 for item in req.items()))
+        self.assertIn("test", req.values())
+        for item in req.items():
+            self.assertEqual(len(item), 2, item)
 
 
 class TestPageGenerator(PywikibotTestCase):
@@ -70,7 +71,7 @@ class TestPageGenerator(PywikibotTestCase):
         for page in results:
             self.assertEqual(type(page), pywikibot.Page)
             self.assertEqual(page.site, mysite)
-            self.assert_(page.title() in titles)
+            self.assertIn(page.title(), titles)
 
 
 class TestCachedRequest(unittest.TestCase):
