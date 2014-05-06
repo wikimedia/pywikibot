1ea38468bfe7a31b43be7b15e798f36df0344e86
Mpaa <mpaa.wiki@gmail.com>
2014-05-06 23:28:50 +0200

    Bug 64855 - get.py not ported from compat but added to listpages.py
    Optionally, page content is retrieved and written to standard output.
    This makes it possible to pipe the text to another process.
    
    Change-Id: Ibd4717af9763d4989e36902b0210f411709f9f48

--- scripts/listpages.py
+++ scripts/listpages.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8  -*-
 """
-Print a list of pages, as defined by page generator parameters
+Print a list of pages, as defined by page generator parameters.
+Optionally, it also prints page content to STDOUT.
 
 These parameters are supported to specify which pages titles to print:
 
 &params;
+
+-notitle          Page title is not printed.
+
+-get              Page content is printed.
+
 """
 #
 # (C) Pywikibot team, 2008-2014
@@ -22,15 +28,26 @@ docuReplacements = {'&params;': parameterHelp}
 
 def main(*args):
     gen = None
+    notitle = False
+    page_get = False
+
     genFactory = GeneratorFactory()
     for arg in pywikibot.handleArgs(*args):
-        genFactory.handleArg(arg)
+        if arg == '-notitle':
+            notitle = True
+        elif arg == '-get':
+            page_get = True
+        else:
+            genFactory.handleArg(arg)
+
     gen = genFactory.getCombinedGenerator()
     if gen:
-        i = 0
-        for page in gen:
-            i += 1
-            pywikibot.stdout("%4d: %s" % (i, page.title()))
+        for i, page in enumerate(gen, start=1):
+            if not notitle:
+                pywikibot.stdout("%4d: %s" % (i, page.title()))
+            if page_get:
+                # TODO: catch exceptions
+                pywikibot.output(page.text, toStdout=True)
     else:
         pywikibot.showHelp()
 
