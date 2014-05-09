67e113d038d08e07b3eb48a60e3fd4ee48461ac6
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2014-05-09 21:51:54 +0200

    Bug 65089: add 'shell' script
    Change-Id: I5f021318a962cfe7815dfc2a71823664061254db

---
+++ scripts/shell.py
@@ -0,0 +1,10 @@
+"""
+Spawns an interactive Python shell.
+
+Usage:
+    python pwb.py shell
+"""
+
+if __name__ == "__main__":
+    import code
+    code.interact("""Welcome to the Pywikibot interactive shell!""")
