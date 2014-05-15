b8cf80df38428c4bfde31499c86f34029ea44d53
xqt <info@gno.de>
2014-05-15 14:42:58 +0200

    Add ID and license
    Change-Id: I5ea2bd526e497a510d1199045861906d84965823

--- scripts/shell.py
+++ scripts/shell.py
@@ -1,9 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """
 Spawns an interactive Python shell.
 
 Usage:
     python pwb.py shell
+
 """
+# (C) Pywikibot team, 2014
+#
+# Distributed under the terms of the MIT license.
+#
+__version__ = '$Id$'
+#
 
 if __name__ == "__main__":
     import code
