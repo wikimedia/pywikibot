wikimedia/pywikibot

commit d51e84906fac543f852483f12c7fc0624aa1ba83

Don't crash when the bot hits an unknown globe
bug: 64495
Change-Id: Ie261991717bf5876054b30da6fcc6574b35ebe00

scripts/coordinate_import.py
@@ -62,11 +62,14 @@ class CoordImportRobot(WikidataBot):
                         newclaim = pywikibot.Claim(self.repo, u'P625')
                         newclaim.setTarget(coordinate)
                         pywikibot.output(u'Adding %s, %s to %s' % (coordinate.lat, coordinate.lon, item.title()))
-                        item.addClaim(newclaim)
+                        try:
+                            item.addClaim(newclaim)
 
-                        source = self.getSource(page.site)
-                        if source:
-                            newclaim.addSource(source, bot=True)
+                            source = self.getSource(page.site)
+                            if source:
+                                newclaim.addSource(source, bot=True)
+                        except CoordinateGlobeUnknownException as e:
+                            pywikibot.output(u'Skipping unsupported globe: %s' % e.args)
 
 
 def main():

pywikibot/__init__.py
@@ -156,7 +156,7 @@ class Coordinate(object):
         FIXME Should this be in the DataSite object?
         """
         if self.globe not in self.site.globes():
-            raise NotImplementedError(u"%s is not supported in Wikibase yet." % self.globe)
+            raise CoordinateGlobeUnknownException(u"%s is not supported in Wikibase yet." % self.globe)
         return {'latitude': self.lat,
                 'longitude': self.lon,
                 'altitude': self.alt,

pywikibot/exceptions.py
@@ -156,3 +156,11 @@ class AutoblockUser(Error):
 
 class UserActionRefuse(Error):
     pass
+
+
+class WikiBaseError(Error):
+    pass
+
+
+class CoordinateGlobeUnknownException(WikiBaseError, NotImplementedError):
+    """ This globe is not implemented yet in either WikiBase or pywikibot """

commit ad89201c82b06758ef7f795c224fd710800e9f2e

Bug 64495: coordinate_import.py: import exception
import the new exception CoordinateGlobeUnknownException
to fix compile error introduced in d51e849

Change-Id: Iee8193a4c9a960831790806588e275d34f8aa9b3

scripts/coordinate_import.py
@@ -26,6 +26,7 @@ __version__ = '$Id$'
 #
 import pywikibot
 from pywikibot import pagegenerators, WikidataBot
+from pywikibot.exceptions import CoordinateGlobeUnknownException
 
 
 class CoordImportRobot(WikidataBot):