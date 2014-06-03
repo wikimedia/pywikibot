35916f347286bc0341dc60b66a9de708a8142715
xqt <info@gno.de>
2014-06-03 10:38:21 +0200

    code cleanup, nothing to import
    remove obsolete doc strings
    
    Change-Id: I0db93ff68cc1a80bbe4efa2110907dd624cf9f89

--- pywikibot/data/__init__.py
+++ pywikibot/data/__init__.py
@@ -3,11 +3,8 @@
 The "data" module provides several physical layers of data access to the wiki.
 """
 #
-# (C) Pywikipedia bot team, 2007
+# (C) Pywikibot team, 2007-2014
 #
 # Distributed under the terms of the MIT license.
 #
 __version__ = '$Id$'
-
-# Import the classes exposed by this module:
-# TODO
