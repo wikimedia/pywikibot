82d42a28422e90aa50b1a9bb1e98955d22a84c1e
xqt <info@gno.de>
2014-06-05 07:52:04 +0200

    Progressing dots to show test is running
    - use a metaclass for this test
    - register all tests in different methods and run them
      separately
    
    This test may be called by unittest.main() whereas the previous test 
    needs unittest.TextTestRunner which isn't called by the automatic
    testing.
    
    Change-Id: I01b3a7ca47ec7fcc3adf02285c803e934c67d52e
    Original-Change-Id: I30a7256f2b87e7e8a348731526e2d36b42a86f9a

--- tests/date_tests.py
+++ tests/date_tests.py
@@ -10,30 +10,48 @@ from tests.utils import unittest
 from pywikibot import date
 
 
+class TestDateMeta(type):
+    """Test meta class"""
+
+    def __new__(cls, name, bases, dct):
+        """create the new class"""
+
+        def test_method(formatname):
+
+            def testMapEntry(self):
+                """The test ported from date.py"""
+                step = 1
+                if formatname in date.decadeFormats:
+                    step = 10
+                try:
+                    predicate, start, stop = date.formatLimits[formatname]
+                except KeyError:
+                    return
+
+                for code, convFunc in date.formats[formatname].items():
+                    for value in range(start, stop, step):
+                        self.assertTrue(
+                            predicate(value),
+                            "date.formats['%(formatname)s']['%(code)s']:\n"
+                            "invalid value %(value)d" % locals())
+
+                        newValue = convFunc(convFunc(value))
+                        self.assertEqual(
+                            newValue, value,
+                            "date.formats['%(formatname)s']['%(code)s']:\n"
+                            "value %(newValue)d does not match %(value)s"
+                            % locals())
+            return testMapEntry
+
+        for formatname in date.formats:
+            test_name = "test_" + formatname
+            dct[test_name] = test_method(formatname)
+        return type.__new__(cls, name, bases, dct)
+
+
 class TestDate(unittest.TestCase):
-    """Test cases for date library"""
-
-    def testMapEntry(self):
-        """Test the validity of the pywikibot.date format maps."""
-        for formatName in date.formats:
-            step = 1
-            if formatName in date.decadeFormats:
-                step = 10
-            predicate, start, stop = date.formatLimits[formatName]
-
-            for code, convFunc in date.formats[formatName].items():
-                for value in range(start, stop, step):
-                    self.assertTrue(
-                        predicate(value),
-                        "date.formats['%(formatName)s']['%(code)s']:\n"
-                        "invalid value %(value)d" % locals())
-
-                    newValue = convFunc(convFunc(value))
-                    self.assertEqual(
-                        newValue, value,
-                        "date.formats['%(formatName)s']['%(code)s']:\n"
-                        "value %(newValue)d does not match %(value)s"
-                        % locals())
+    """Test cases for date library processed by unittest"""
+    __metaclass__ = TestDateMeta
 
 
 if __name__ == '__main__':
