d65630df2d3dc9fdb684cc38038ea2d421015fc0
xqt <info@gno.de>
2014-07-27 16:13:47 +0200

    update mw version
    Change-Id: I12ee3d8185c3783070e5204b0070eb1acf1ee760

--- pywikibot/family.py
+++ pywikibot/family.py
@@ -1088,7 +1088,7 @@ class WikimediaFamily(Family):
 
         """
         # Here we return the latest mw release of wikimedia projects
-        return '1.24wmf11'
+        return '1.24wmf14'
 
     def shared_image_repository(self, code):
         return ('commons', 'commons')
