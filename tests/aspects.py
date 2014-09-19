44da15ddedc32792195672b6615ba8f2a1a876aa
John Vandenberg <jayvdb@gmail.com>
2014-09-19 13:20:06 +1000

    Preload connected ItemPage for Page objects
    DataSite.preloaditems currently require an ItemPage.  It should accept
    Page object for an item, or Page objects for client pages connected
    to an item.
    
    Change-Id: Ibca36593333ed99117aded2a0da3eaf7218a44c6

--- tests/aspects.py
+++ tests/aspects.py
@@ -599,6 +599,14 @@ class WikibaseClientTestCase(WikibaseTestCase):
                     % (cls.__name__, site['site']))
 
 
+class DefaultWikibaseClientTestCase(WikibaseClientTestCase,
+                                    DefaultSiteTestCase):
+
+    """Run tests against any site connected to a Wikibase."""
+
+    pass
+
+
 class WikidataTestCase(WikibaseTestCase):
 
     """Test cases use Wikidata."""
@@ -607,6 +615,25 @@ class WikidataTestCase(WikibaseTestCase):
     code = 'wikidata'
 
     cached = True
+
+
+class DefaultWikidataClientTestCase(DefaultWikibaseClientTestCase):
+
+    """Run tests against any site connected to Wikidata."""
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up the test class.
+
+        Require the data repository is wikidata.org.
+        """
+        super(WikibaseClientTestCase, cls).setUpClass()
+
+        if str(cls.get_repo()) != 'wikidata:wikidata':
+            raise unittest.SkipTest(
+                u'%s: %s is not connected to Wikidata.'
+                % (cls.__name__, cls.get_site()))
 
 
 class PwbTestCase(TestCase):
