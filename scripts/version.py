182616b47b795d76151ee8f6bf56995ba730915e
John Vandenberg <jayvdb@gmail.com>
2014-10-20 19:36:09 +0700

    Add cert test to version script
    Each platform may patch the httplib2 variable CA_CERTS, and
    httplib2 allows this variable to be dynamically patched by
    package 'httplib2.ca_certs_locater'.
    
    To simplify debugging of ssl certificate verification errors,
    it is useful to know which file is being loaded, and whether it
    includes the certificate needed for Wikimedia projects.
    
    Bug 72009
    
    Change-Id: Ib3d1c5e1b5d683204150b04f3df4176754fbaaa5

--- scripts/version.py
+++ scripts/version.py
@@ -12,16 +12,30 @@ __version__ = '$Id$'
 #
 
 import sys
+import os
 import pywikibot
 from pywikibot.version import getversion
+try:
+    import httplib2
+except ImportError:
+    httplib2 = {'__version__': 'n/a'}
 
 if __name__ == '__main__':
     pywikibot.output('Pywikibot: %s' % getversion())
     pywikibot.output('Release version: %s' % pywikibot.__release__)
+    pywikibot.output('httplib2 version: %s' % httplib2.__version__)
+    if not hasattr(httplib2, 'CA_CERTS'):
+        httplib2.CA_CERTS = ''
+    pywikibot.output('  cacerts: %s' % httplib2.CA_CERTS)
+    has_wikimedia_cert = False
+    if os.path.isfile(httplib2.CA_CERTS):
+        with open(httplib2.CA_CERTS, 'r') as cert_file:
+            text = cert_file.read()
+            if 'MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs' in text:
+                has_wikimedia_cert = True
+    pywikibot.output(u'    certificate test: %s' % ('ok' if has_wikimedia_cert else 'not ok'))
     pywikibot.output('Python: %s' % sys.version)
     if not __import__('unicodedata').normalize('NFC', u'\u092e\u093e\u0930\u094d\u0915 \u091c\u093c\u0941\u0915\u0947\u0930\u092c\u0930\u094d\u0917') == u'\u092e\u093e\u0930\u094d\u0915 \u091c\u093c\u0941\u0915\u0947\u0930\u092c\u0930\u094d\u0917':
-        pywikibot.output(u'unicode test: triggers problem #3081100')
+        pywikibot.output(u'  unicode test: triggers problem #3081100')
     else:
-        pywikibot.output(u'unicode test: ok')
-    import httplib2
-    pywikibot.output('httplib2 version: %s' % httplib2.__version__)
+        pywikibot.output(u'  unicode test: ok')
