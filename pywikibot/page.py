74549160a2f3ef91cff80a3c9a59a9815df85785
Mpaa <mpaa.wiki@gmail.com>
2015-02-12 22:03:44 +0100

    page.py: Add string methods to class Revision
    Added:
    - Revision.__unicode__()
    - Revision.__repr__()
    
    Change-Id: Ie0ad1aa6ca30468b288502d12c280e70c20a5420

--- pywikibot/page.py
+++ pywikibot/page.py
@@ -4144,7 +4144,7 @@ class Claim(Property):
                 }
 
 
-class Revision(object):
+class Revision(pywikibot.UnicodeMixin):
 
     """A structure holding information about a single revision of a Page."""
 
@@ -4212,6 +4212,16 @@ class Revision(object):
 
         """
         return getattr(self, key)
+
+    def __unicode__(self):
+        """Return string representation."""
+        _content = u', '.join(
+            u'{0}: {1}'.format(k, v) for k, v in self.__dict__.items())
+        return u'{{{0}}}'.format(_content)
+
+    def __repr__(self):
+        """Return a more complete string representation."""
+        return self.__dict__.__repr__()
 
 
 class FileInfo(pywikibot.UnicodeMixin):
