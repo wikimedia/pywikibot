wikimedia/pywikibot

commit bc9dd662afcaa8e11b911fd4f8d2570d2747ed54

[FEAT/FIX] Use input_choice or new input_yn
As inputChoice has been deprecated this updates all references to the
replacement (input_choice).

Also added input_yn which is like input_choice but returns a boolean and
automatically sets the answers to [y]es and [n]o.

Change-Id: Ib68e66a60e5bc5a78602253123312a380109657d

scripts/selflink.py
@@ -55,7 +55,6 @@ class SelflinkBot(Bot):
             r'(?P<section>#[^\]\|]*)?'
             '(\|(?P<label>[^\]]*))?\]\]'
             r'(?P<linktrail>' + linktrail + ')')
-        self.done = False
 
     def handleNextLink(self, page, match, context=100):
         """Process the next link on a page, offering the user choices.
@@ -97,11 +96,10 @@ class SelflinkBot(Bot):
             matchText = match.group(0)
             pywikibot.output(
                 pre + '\03{lightred}' + matchText + '\03{default}' + post)
-            choice = pywikibot.inputChoice(
+            choice = pywikibot.input_choice(
                 u'\nWhat shall be done with this selflink?\n',
-                ['unlink', 'make bold', 'skip', 'edit', 'more context',
-                 'unlink all', 'quit'],
-                ['U', 'b', 's', 'e', 'm', 'a', 'q'], 'u')
+                [('unlink', 'u'), ('make bold', 'b'), ('skip', 's'),
+                 ('edit', 'e'), ('more context', 'm'), ('unlink all')], 'u')
             pywikibot.output(u'')
 
             if choice == 's':
@@ -121,9 +119,6 @@ class SelflinkBot(Bot):
                 return self.handleNextLink(page, match, context=context + 100)
             elif choice == 'a':
                 self.always = True
-            elif choice == 'q':
-                self.done = True
-                return False
 
         # choice was 'U', 'b', or 'a'
         new = match.group('label') or match.group('title')
@@ -151,7 +146,7 @@ class SelflinkBot(Bot):
                     % page.title(asLink=True))
                 return
             curpos = 0
-            while curpos < len(page.text) or self.done:
+            while curpos < len(page.text):
                 match = self.linkR.search(page.text, pos=curpos)
                 if not match:
                     break
@@ -176,12 +171,6 @@ class SelflinkBot(Bot):
                              % page.title(asLink=True))
         except pywikibot.LockedPage:
             pywikibot.output(u"Page %s is locked." % page.title(asLink=True))
-
-    def run(self):
-        for page in self.generator:
-            if self.done:
-                break
-            self.treat(page)
 
 
 def main(*args):

generate_user_files.py
@@ -98,7 +98,7 @@ your operating system. See your operating system documentation for how to
 set environment variables.""" % locals(), width=76)
     for line in msg:
         pywikibot.output(line)
-    if pywikibot.inputChoice("Is this OK?", ["yes", "no"], ["y", "n"], "n") == "y":
+    if pywikibot.input_yn('Is this OK?', default=False, automatic_quit=False):
         return new_base
     pywikibot.output("Aborting changes.")
     return False
@@ -170,10 +170,10 @@ def get_site_and_lang(default_family='wikipedia', default_lang='en',
     while not mylang:
         mylang = pywikibot.input(message) or default_lang
         if known_langs and mylang and mylang not in known_langs:
-            if pywikibot.inputChoice("The language code {0} is not in the "
-                                     "list of known languages. Do you want to "
-                                     "continue?".format(mylang),
-                                     ["yes", "no"], ["y", "n"], "n") == "n":
+            if not pywikibot.input_yn("The language code {0} is not in the "
+                                      "list of known languages. Do you want "
+                                      "to continue?".format(mylang),
+                                      default=False, automatic_quit=False):
                 mylang = None
 
     username = None
@@ -250,8 +250,8 @@ def create_user_config():
         main_family, main_lang, main_username = get_site_and_lang()
 
         usernames = [(main_family, main_lang, main_username)]
-        while pywikibot.inputChoice("Do you want to add any other projects?",
-                                    ["yes", "no"], ["y", "n"], "n") == "y":
+        while pywikibot.input_yn("Do you want to add any other projects?",
+                                 default=False, automatic_quit=False):
             usernames += [get_site_and_lang(main_family, main_lang,
                                             main_username)]
 
@@ -259,9 +259,9 @@ def create_user_config():
             u"usernames['{0}']['{1}'] = u'{2}'".format(*username)
             for username in usernames)
 
-        extended = pywikibot.inputChoice(
-            "Which variant of user_config.py?",
-            ["small", "extended (with further information)"], ["s", "e"]) == "e"
+        extended = pywikibot.input_yn("Would you like the extended version of "
+                                      "user-config.py, with explanations "
+                                      "included?", automatic_quit=False)
 
         if extended:
             # config2.py will be in the pywikibot/ directory relative to this
@@ -321,8 +321,8 @@ fixes['example'] = {
 if __name__ == "__main__":
     while True:
         pywikibot.output(u'\nYour default user directory is "%s"' % base_dir)
-        if pywikibot.inputChoice("How to proceed?", ["keep", "change"],
-                                 ["k", "c"], "k") == "c":
+        if pywikibot.input_yn("Do you want to use that directory?",
+                              default=False, automatic_quit=False):
             new_base = change_base_dir()
             if new_base:
                 base_dir = new_base
@@ -335,9 +335,10 @@ if __name__ == "__main__":
     while True:
         if os.path.exists(os.path.join(base_dir, "user-config.py")):
             break
-        if pywikibot.inputChoice(
+        if pywikibot.input_yn(
                 "Do you want to copy user files from an existing Pywikibot "
-                "installation?", ["yes", "no"], ["y", "n"]) == "y":
+                "installation?",
+                automatic_quit=False):
             oldpath = pywikibot.input("Path to existing user-config.py?")
             if not os.path.exists(oldpath):
                 pywikibot.error("Not a valid path")
@@ -361,16 +362,16 @@ if __name__ == "__main__":
         else:
             break
     if not os.path.isfile(os.path.join(base_dir, "user-config.py")):
-        if pywikibot.inputChoice("Create user-config.py file? Required for "
-                                 "running bots.",
-                                 ["yes", "no"], ["y", "n"], "n") == "y":
+        if pywikibot.input_yn('Create user-config.py file? Required for '
+                              'running bots.',
+                              default=False, automatic_quit=False):
             create_user_config()
     elif not copied_config:
         pywikibot.output("user-config.py already exists in the directory")
     if not os.path.isfile(os.path.join(base_dir, "user-fixes.py")):
-        if pywikibot.inputChoice("Create user-fixes.py file? Optional and for "
-                                 "advanced users.",
-                                 ["yes", "no"], ["y", "n"], "n") == "y":
+        if pywikibot.input_yn('Create user-fixes.py file? Optional and for '
+                              'advanced users.',
+                              default=False, automatic_quit=False):
             create_user_fixes()
     elif not copied_fixes:
         pywikibot.output("user-fixes.py already exists in the directory")

pywikibot/__init__.py
@@ -29,7 +29,7 @@ else:
 from pywikibot import config2 as config
 from pywikibot.bot import (
     output, warning, error, critical, debug, stdout, exception,
-    input, input_choice, inputChoice, handle_args, showHelp, ui, log,
+    input, input_choice, input_yn, inputChoice, handle_args, showHelp, ui, log,
     calledModuleName, Bot, WikidataBot, QuitKeyboardInterrupt,
     # the following are flagged as deprecated on usage
     handleArgs,
@@ -68,7 +68,7 @@ __all__ = ('config', 'ui', 'UnicodeMixin', 'translate',
            'ItemPage', 'PropertyPage', 'Claim', 'TimeStripper',
            'html2unicode', 'url2unicode', 'unicode2html',
            'stdout', 'output', 'warning', 'error', 'critical', 'debug',
-           'exception', 'input_choice', 'input', 'inputChoice',
+           'exception', 'input_choice', 'input', 'input_yn', 'inputChoice',
            'handle_args', 'handleArgs', 'showHelp', 'ui', 'log',
            'calledModuleName', 'Bot', 'WikidataBot',
            'Error', 'InvalidTitle', 'BadTitle', 'NoPage', 'SectionError',
@@ -699,11 +699,9 @@ def stopme():
             try:
                 _putthread.join(1)
             except KeyboardInterrupt:
-                answer = inputChoice(u"""\
-There are %i pages remaining in the queue. Estimated time remaining: %s
-Really exit?""" % remaining(),
-                    ['yes', 'no'], ['y', 'N'], 'N')
-                if answer == 'y':
+                if input_yn('There are %i pages remaining in the queue. '
+                            'Estimated time remaining: %s\nReally exit?'
+                            % remaining(), default=False, automatic_quit=False):
                     return
 
     # only need one drop() call because all throttles use the same global pid

pywikibot/bot.py
@@ -516,6 +516,8 @@ def input_choice(question, answers, default=None, return_shortcut=True,
     """
     Ask the user the question and return one of the valid answers.
 
+    @param question: The question asked without trailing spaces.
+    @type answers: basestring
     @param answers: The valid answers each containing a full length answer and
         a shortcut. Each value must be unique.
     @type answers: Iterable containing an iterable of length two
@@ -527,7 +529,7 @@ def input_choice(question, answers, default=None, return_shortcut=True,
         returned.
     @type return_shortcut: bool
     @param automatic_quit: Adds the option 'Quit' ('q') and throw a
-            L{QuitKeyboardInterrupt} if selected (default).
+            L{QuitKeyboardInterrupt} if selected.
     @type automatic_quit: bool
     @return: The selected answer shortcut or index. Is -1 if the default is
         selected, it does not return the shortcut and the default is not a
@@ -540,6 +542,34 @@ def input_choice(question, answers, default=None, return_shortcut=True,
 
     return ui.input_choice(question, answers, default, return_shortcut,
                            automatic_quit)
+
+
+def input_yn(question, default=None, automatic_quit=True):
+    """
+    Ask the user a yes/no question and returns the answer as a bool.
+
+    @param question: The question asked without trailing spaces.
+    @type answers: basestring
+    @param default: The result if no answer was entered. It must be a bool or
+        'y' or 'n' and can be disabled by setting it to None.
+    @type default: basestring or bool
+    @param automatic_quit: Adds the option 'Quit' ('q') and throw a
+            L{QuitKeyboardInterrupt} if selected.
+    @type automatic_quit: bool
+    @return: Return True if the user selected yes and False if the user
+        selected no. If the default is not None it'll return True if default
+        is True or 'y' and False if default is False or 'n'.
+    @rtype: bool
+    """
+    if default not in ['y', 'Y', 'n', 'N']:
+        if default:
+            default = 'y'
+        elif default is not None:
+            default = 'n'
+    assert default in ['y', 'Y', 'n', 'N', None]
+
+    return input_choice(question, [('Yes', 'y'), ('No', 'n')], default,
+                        automatic_quit=automatic_quit) == 'y'
 
 
 @deprecated('input_choice')

pywikibot/diff.py
@@ -245,8 +245,8 @@ class PatchManager(object):
                     ]
 
         question = 'Accept this hunk?'
-        answers = ['yes', 'no', 'stop', 'all', 'review', 'help']
-        hotkeys = ['y', 'n', 's', 'a', 'r', 'h']
+        answers = [('yes', 'y'), ('no', 'n'), ('stop', 's'), ('all', 'a'),
+                   ('review', 'r'), ('help', 'h')]
         actions = {'y': Hunk.APPR,
                    'n': Hunk.NOT_APPR,
                    's': Hunk.NOT_APPR,
@@ -261,7 +261,8 @@ class PatchManager(object):
             hunk = pending.pop(0)
 
             pywikibot.output(hunk.header + hunk.diff_text)
-            choice = pywikibot.inputChoice(question, answers, hotkeys, default='r')
+            choice = pywikibot.input_choice(question, answers, default='r',
+                                            automatic_quit=False)
 
             if choice in actions.keys():
                 hunk.reviewed = actions[choice]

pywikibot/page.py
@@ -1476,12 +1476,11 @@ class Page(pywikibot.UnicodeMixin, ComparableMixin):
         if self.site.username(sysop=True):
             answer = u'y'
             if prompt and not hasattr(self.site, '_noDeletePrompt'):
-                answer = pywikibot.inputChoice(
+                answer = pywikibot.input_choice(
                     u'Do you want to delete %s?' % self.title(
                         asLink=True, forceInterwiki=True),
-                    ['Yes', 'No', 'All'],
-                    ['y', 'n', 'a'],
-                    'n')
+                    [('Yes', 'y'), ('No', 'n'), ('All', 'a')],
+                    'n', automatic_quit=False)
                 if answer == 'a':
                     answer = 'y'
                     self.site._noDeletePrompt = True
@@ -1491,13 +1490,12 @@ class Page(pywikibot.UnicodeMixin, ComparableMixin):
             if mark or hasattr(self.site, '_noMarkDeletePrompt'):
                 answer = 'y'
             else:
-                answer = pywikibot.inputChoice(
+                answer = pywikibot.input_choice(
                     u"Can't delete %s; do you want to mark it "
                     "for deletion instead?" % self.title(asLink=True,
                                                          forceInterwiki=True),
-                    ['Yes', 'No', 'All'],
-                    ['y', 'n', 'a'],
-                    'n')
+                    [('Yes', 'y'), ('No', 'n'), ('All', 'a')],
+                    'n', automatic_quit=False)
                 if answer == 'a':
                     answer = 'y'
                     self.site._noMarkDeletePrompt = True
@@ -1651,12 +1649,11 @@ class Page(pywikibot.UnicodeMixin, ComparableMixin):
             pywikibot.bot.warning(u'"prompt" argument of protect() is '
                                   'deprecated')
         if prompt and not hasattr(self.site, '_noProtectPrompt'):
-            answer = pywikibot.inputChoice(
+            answer = pywikibot.input_choice(
                 u'Do you want to change the protection level of %s?'
                 % self.title(asLink=True, forceInterwiki=True),
-                ['Yes', 'No', 'All'],
-                ['y', 'N', 'a'],
-                'N')
+                [('Yes', 'y'), ('No', 'n'), ('All', 'a')],
+                'n', automatic_quit=False)
             if answer == 'a':
                 answer = 'y'
                 self.site._noProtectPrompt = True

scripts/add_text.py
@@ -222,10 +222,10 @@ Match was: %s''' % result)
         # text in the page
         if putText:
             if not always:
-                choice = pywikibot.inputChoice(
+                choice = pywikibot.input_choice(
                     u'Do you want to accept these changes?',
-                    ['Yes', 'No', 'All', 'open in Browser'],
-                    ['y', 'n', 'a', 'b'], 'n')
+                    [('Yes', 'y'), ('No', 'n'), ('All', 'a'),
+                     ('open in Browser', 'b')], 'n', automatic_quit=False)
                 if choice == 'a':
                     always = True
                 elif choice == 'n':

scripts/basic.py
@@ -111,10 +111,9 @@ class BasicBot:
             pywikibot.showDiff(page.get(), text)
             pywikibot.output(u'Comment: %s' % comment)
             if not self.dry:
-                choice = pywikibot.inputChoice(
-                    u'Do you want to accept these changes?',
-                    ['Yes', 'No'], ['y', 'N'], 'N')
-                if choice == 'y':
+                if pywikibot.input_yn(
+                        u'Do you want to accept these changes?',
+                        default=False, automatic_quit=False):
                     try:
                         page.text = text
                         # Save the page

commit fd3341c88da222f04667a77c7e2b8011eb02e9f8

[FIX] selflink: Correctly call input_choice
The input_choice call in selflink is incorrect as it doesn't provide a shortcut
for 'unlink all' which was introduced in bc9dd662.

Change-Id: I0a1a9cad0040406a9ecab6b2b7de640a65883662

scripts/selflink.py
@@ -100,7 +100,7 @@ class SelflinkBot(SingleSiteBot):
             choice = pywikibot.input_choice(
                 u'\nWhat shall be done with this selflink?\n',
                 [('unlink', 'u'), ('make bold', 'b'), ('skip', 's'),
-                 ('edit', 'e'), ('more context', 'm'), ('unlink all')], 'u')
+                 ('edit', 'e'), ('more context', 'm'), ('unlink all', 'a')], 'u')
             pywikibot.output(u'')
 
             if choice == 's':