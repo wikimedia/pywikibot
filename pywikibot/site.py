wikimedia/pywikibot

commit d4e6e60c3e0423fb4e2ff54e543ea187167429c3

Verifies tokens from paraminfo
Resolves the API warnings due to tokens which only concern global accounts.

Instead of hard coded variables TOKENS_[012] , now using the following:

For mwVersion < 1.20 gets the tokens from action=paraminfo&querymodules=info
For mwVersion < 1.24wmf19 and >= 1.20 from action=paraminfo&modules=tokens
For mwVersion > 1.24wmf19 from action=paraminfo&querymodules=tokens
and replacing those required and present in action=paraminfo&querymodules=info
or action=paraminfo&modules=tokens by 'csrf'

Bug: T72965
Bug: T85725
Change-Id: I3ff70dd8b9ee33fde15bd13d7af15db408aefc7d

pywikibot/site.py
@@ -1492,50 +1492,6 @@ class APISite(BaseSite):
 #    Pages; see method docs for details) --
 #
 
-    # Constants for token management.
-    # For all MediaWiki versions prior to 1.20.
-    # 'patrol' is indirectly supported via 'edit' token or recentchanges.
-    # It will be converted in site.validate_tokens()/site.get_tokens().
-    TOKENS_0 = set(['edit',
-                    'delete',
-                    'protect',
-                    'move',
-                    'block',
-                    'unblock',
-                    'email',
-                    'import',
-                    'watch',
-                    'patrol',
-                    ])
-
-    # For all MediaWiki versions, with 1.20 <= version < 1.24wmf19
-    TOKENS_1 = set(['block',
-                    'centralauth',
-                    'delete',
-                    'deleteglobalaccount',
-                    'undelete',
-                    'edit',
-                    'email',
-                    'import',
-                    'move',
-                    'options',
-                    'patrol',
-                    'protect',
-                    'setglobalaccountstatus',
-                    'unblock',
-                    'watch',
-                    ])
-
-    # For all MediaWiki versions >= 1.24wmf19
-    TOKENS_2 = set(['csrf',
-                    'deleteglobalaccount',
-                    'patrol',
-                    'rollback',
-                    'setglobalaccountstatus',
-                    'userrights',
-                    'watch',
-                    ])
-
     def __init__(self, code, fam=None, user=None, sysop=None):
         """Constructor."""
         BaseSite.__init__(self, code, fam, user, sysop)
@@ -2646,7 +2602,10 @@ class APISite(BaseSite):
         """
         _version = MediaWikiVersion(self.version())
         if _version < MediaWikiVersion('1.20'):
-            valid_types = [token for token in types if token in self.TOKENS_0]
+            types_wiki = self._paraminfo.parameter('query+info',
+                                                   'token')['type']
+            types_wiki.append('patrol')
+            valid_types = [token for token in types if token in types_wiki]
 
             # Pre 1.17, preload token was the same as the edit token.
             if _version < MediaWikiVersion('1.17'):
@@ -2654,16 +2613,22 @@ class APISite(BaseSite):
                     valid_types.append('edit')
 
         elif _version < MediaWikiVersion('1.24wmf19'):
-            valid_types = [token for token in types if token in self.TOKENS_1]
+            types_wiki = self._paraminfo.parameter('tokens',
+                                                   'type')['type']
+            valid_types = [token for token in types if token in types_wiki]
         else:
-            valid_types = []
+            types_wiki_old = self._paraminfo.parameter('query+info',
+                                                       'token')['type']
+            types_wiki_action = self._paraminfo.parameter('tokens',
+                                                          'type')['type']
+            types_wiki = self._paraminfo.parameter('query+tokens',
+                                                   'type')['type']
+            valid_types = [token for token in types if token in types_wiki]
             for token in types:
-                if ((token in self.TOKENS_0 or token in self.TOKENS_1) and
-                        token not in self.TOKENS_2):
-                    token = 'csrf'
-                if token in self.TOKENS_2:
-                    valid_types.append(token)
-
+                if (token not in valid_types and
+                        (token in types_wiki_old or
+                         token in types_wiki_action)):
+                    valid_types.append('csrf')
         return valid_types
 
     def get_tokens(self, types, all=False):
@@ -2708,7 +2673,9 @@ class APISite(BaseSite):
         _version = MediaWikiVersion(self.version())
         if _version < MediaWikiVersion('1.20'):
             if all:
-                types.extend(self.TOKENS_0)
+                types_wiki = self._paraminfo.parameter('query+info',
+                                                       'token')['type']
+                types.extend(types_wiki)
             valid_tokens = set(self.validate_tokens(types))
             # don't request patrol
             query = api.PropertyGenerator('info',
@@ -2746,12 +2713,16 @@ class APISite(BaseSite):
         else:
             if _version < MediaWikiVersion('1.24wmf19'):
                 if all is not False:
-                    types.extend(self.TOKENS_1)
+                    types_wiki = self._paraminfo.parameter('action+tokens',
+                                                           'type')['type']
+                    types.extend(types_wiki)
                 req = api.Request(site=self, action='tokens',
                                    type='|'.join(self.validate_tokens(types)))
             else:
                 if all is not False:
-                    types.extend(self.TOKENS_2)
+                    types_wiki = self._paraminfo.parameter('query+tokens',
+                                                           'type')['type']
+                    types.extend(types_wiki)
 
                 req = api.Request(site=self, action='query', meta='tokens',
                                    type='|'.join(self.validate_tokens(types)))

commit f79579354aaadbc778d2ed1664cd7a973c5ee60b

[FIX] undelete: Get correct token
The original implementation cdc2e9f4 used `undelete` token and added it as an
old token type. So on new implementations using the CSRF token this worked as
it mapped the token to CSRF. But old versions of MediaWiki which don't use the
CSRF token yet it'll fail because that token never existed.

The change in d4e6e60c broke then this also on newer versions as it only mapped
old token names to the CSRF token if they were actually old token name. And
that is not the case for `undelete` so it wasn't mapped to CSRF.

With 730bad3f the tokens for `delete` and `undelete` are the same and so it can
just use the `delete` token which will be mapped to CSRF on recent versions.

Bug: T105635
Change-Id: I9419aac0bc590fd3a3ed484012608f25dcf01cea

pywikibot/site.py
@@ -4771,7 +4771,7 @@ class APISite(BaseSite):
         @type reason: basestring
 
         """
-        token = self.tokens['undelete']
+        token = self.tokens['delete']
         self.lock_page(page)
 
         req = self._simple_request(action='undelete',