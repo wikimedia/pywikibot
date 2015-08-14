b2307901754b9a9fb2ec02033f181bb1c2c8b396
VcamX <vcamx3@gmail.com>
2015-08-14 23:41:41 +0800

    Implement OAuthEditTest
    OAuthEditTest will login, edit a userpage and confirm the edit was done
    using OAuth.
    
    Bug: T109061
    Change-Id: I1344b4496af9a9ebfb698ebb7e864c61e4833750

--- tests/edit_tests.py
+++ tests/edit_tests.py
@@ -15,8 +15,10 @@ import time
 import pywikibot
 
 from pywikibot import page_put_queue
+from pywikibot import config
 
 from tests.aspects import unittest, TestCase
+from tests.oauth_tests import OAuthSiteTestCase
 
 called_back = False
 
@@ -70,6 +72,43 @@ class TestGeneralWrite(TestCase):
         p = pywikibot.Page(self.site, 'User:John Vandenberg/appendtext test')
         self.assertTrue(p.text.endswith(ts))
         self.assertTrue(p.text != ts)
+
+
+class OAuthEditTest(OAuthSiteTestCase):
+
+    """Run edit test with OAuth enabled."""
+
+    family = 'wikipedia'
+    code = 'test'
+
+    write = True
+    oauth = True
+
+    def setUp(self):
+        """Set up test by checking site and initialization."""
+        super(OAuthEditTest, self).setUp()
+        self._authenticate = config.authenticate
+        oauth_tokens = self.consumer_token + self.access_token
+        config.authenticate[self.site.hostname()] = oauth_tokens
+
+    def tearDown(self):
+        """Tear down test by resetting config.authenticate."""
+        config.authenticate = self._authenticate
+
+    def test_edit(self):
+        """Test editing to a page."""
+        self.site.login()
+        self.assertTrue(self.site.logged_in())
+        ts = str(time.time())
+        p = pywikibot.Page(self.site,
+                           'User:%s/edit test' % self.site.username())
+        p.site.editpage(p, appendtext=ts)
+        revision_id = p.latest_revision_id
+        p = pywikibot.Page(self.site,
+                           'User:%s/edit test' % self.site.username())
+        self.assertEqual(revision_id, p.latest_revision_id)
+        self.assertTrue(p.text.endswith(ts))
+
 
 if __name__ == '__main__':
     try:
