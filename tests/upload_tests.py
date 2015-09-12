ace3ca9c53a1f024fafacfda7313da69926a1e20
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-09-12 22:56:47 +0000

    [FEAT] upload: Get stash info before continuing
    If the response does contain warnings it won't contain the offset for the
    continuation (T73737). So it requests the stash info for the given file key to
    determine the offset. This also allows to properly reuse the file key as it
    will create a new file key instance when the offset is 0. It also prevents
    that the API shows an error if the offset is neither 0 nor the actual size
    uploaded.
    
    And if the offset is given it can verify that it matches the stashed data. It
    also supports comparing the SHA1 of the already uploaded part to minimize the
    chance that a different file is uploaded for continuation. It now also verifies
    that the offset is not larger than the file size. In case the file is large and
    doing an SHA1 would be time consuming it's possible to not verify them.
    
    The offset is not present if the warning occurred in the first chunk which
    would have caused that the first chunk is discarded when continuing to upload.
    To do that the file key must be defined before it issues the warnings.
    
    Also when uploading in the unchunked mode and a file key is provided it did
    upload the complete file even though the offset was set to `False` to indicate
    the file is completely uploaded.
    
    One test is an expected failure due to T112416.
    
    Change-Id: I610a0744d74dc5a2523d96eabde818f76dd6c5e8

--- tests/upload_tests.py
+++ tests/upload_tests.py
@@ -17,6 +17,8 @@ import os
 
 import pywikibot
 
+from pywikibot.data.api import APIError
+
 from tests import _images_dir
 from tests.aspects import unittest, TestCase
 
@@ -29,6 +31,9 @@ class TestUpload(TestCase):
 
     family = 'wikipedia'
     code = 'test'
+
+    sounds_png = os.path.join(_images_dir, 'MP_sounds.png')
+    arrow_png = os.path.join(_images_dir, '1rightarrow.png')
 
     def test_png(self):
         """Test uploading a png using Site.upload."""
@@ -45,6 +50,114 @@ class TestUpload(TestCase):
         self.site.upload(page, source_filename=local_filename,
                          comment='pywikibot test',
                          ignore_warnings=True, chunk_size=1024)
+
+    def _init_upload(self, chunk_size):
+        """Do an initial upload causing an abort because of warnings."""
+        def warn_callback(warnings):
+            """A simple callback not automatically finishing the upload."""
+            self.assertCountEqual([w.code for w in warnings], expected_warns)
+            # by now we know there are only two but just make sure
+            assert len(warnings) == len(expected_warns)
+            assert len(expected_warns) in [1, 2]
+            if len(expected_warns) == 2:
+                self.assertEqual(warnings[0].file_key, warnings[1].file_key)
+                self.assertEqual(warnings[0].offset, warnings[1].offset)
+            self._file_key = warnings[0].file_key
+            self._offset = warnings[0].offset
+
+        if chunk_size:
+            expected_warns = ['exists']
+        else:
+            expected_warns = ['duplicate', 'exists']
+
+        # First upload the warning with warnings enabled
+        page = pywikibot.FilePage(self.site, 'MP_sounds-pwb.png')
+        self.assertFalse(hasattr(self, '_file_key'))
+        self.site.upload(page, source_filename=self.sounds_png,
+                         comment='pywikibot test', chunk_size=chunk_size,
+                         ignore_warnings=warn_callback)
+
+        # Check that the warning happened and it's cached
+        self.assertTrue(hasattr(self, '_file_key'))
+        self.assertIs(self._offset, True)
+        self.assertRegex(self._file_key, r'[0-9a-z]+.[0-9a-z]+.\d+.png')
+        self._verify_stash()
+
+    def _verify_stash(self):
+        info = self.site.stash_info(self._file_key, ['size', 'sha1'])
+        if info['size'] == 1024:
+            self.assertEqual('3503db342c8dfb0a38db0682b7370ddd271fa163',
+                             info['sha1'])
+        else:
+            self.assertEqual('0408a0f6a5e057e701f3aed96b0d1fb913c3d9d0',
+                             info['sha1'])
+
+    def _finish_upload(self, chunk_size, file_name):
+        """Finish the upload."""
+        # Finish/continue upload with the given file key
+        page = pywikibot.FilePage(self.site, 'MP_sounds-pwb.png')
+        self.site.upload(page, source_filename=file_name,
+                         comment='pywikibot test', chunk_size=chunk_size,
+                         ignore_warnings=True, report_success=False,
+                         _file_key=self._file_key, _offset=self._offset)
+
+    def _test_continue_filekey(self, chunk_size):
+        """Test uploading a chunk first and finish in a separate upload."""
+        self._init_upload(chunk_size)
+        self._finish_upload(chunk_size, self.sounds_png)
+
+        # Check if it's still cached
+        with self.assertRaises(APIError) as cm:
+            self.site.stash_info(self._file_key)
+        self.assertEqual(cm.exception.code, 'siiinvalidsessiondata')
+        self.assertTrue(cm.exception.info.startswith('File not found'),
+                        'info ({0}) did not start with '
+                        '"File not found"'.format(cm.exception.info))
+
+    def test_continue_filekey_once(self):
+        """Test continuing to upload a file without using chunked mode."""
+        self._test_continue_filekey(0)
+
+    @unittest.expectedFailure  # see T112416
+    def test_continue_filekey_chunked(self):
+        """Test continuing to upload a file with using chunked mode."""
+        self._test_continue_filekey(1024)
+
+    def test_sha1_missmatch(self):
+        """Test trying to continue with a different file."""
+        self._init_upload(1024)
+        with self.assertRaises(ValueError) as cm:
+            self._finish_upload(1024, self.arrow_png)
+        self.assertEqual(
+            str(cm.exception),
+            'The SHA1 of 1024 bytes of the stashed "{0}" is '
+            '3503db342c8dfb0a38db0682b7370ddd271fa163 while the local file is '
+            '3dd334f11aa1e780d636416dc0649b96b67588b6'.format(self._file_key))
+        self._verify_stash()
+
+    def test_offset_missmatch(self):
+        """Test trying to continue with a different offset."""
+        self._init_upload(1024)
+        self._offset = 0
+        with self.assertRaises(ValueError) as cm:
+            self._finish_upload(1024, self.sounds_png)
+        self.assertEqual(
+            str(cm.exception),
+            'For the file key "{0}" the server reported a size 1024 while the '
+            'offset was 0'.format(self._file_key))
+        self._verify_stash()
+
+    def test_offset_oversize(self):
+        """Test trying to continue with an offset which is to large."""
+        self._init_upload(1024)
+        self._offset = 2000
+        with self.assertRaises(ValueError) as cm:
+            self._finish_upload(1024, self.sounds_png)
+        self.assertEqual(
+            str(cm.exception),
+            'For the file key "{0}" the offset was set to 2000 while the file '
+            'is only 1276 bytes large.'.format(self._file_key))
+        self._verify_stash()
 
 
 if __name__ == '__main__':
