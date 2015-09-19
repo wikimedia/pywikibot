38ae920a24be3e7c7b40e943a1f5ccb46067d4a6
lokal-profil <lokal.profil@gmail.com>
2015-09-19 19:49:17 +0200

    Update revId upon claim change
    changeTarget, removeSources: Prevents edit conflicts
    addSources: No need to set every time through the loop
    
    Change-Id: I2d8b69a3894aaf6d58a62ecba48f5dc584617e82

--- pywikibot/page.py
+++ pywikibot/page.py
@@ -4259,6 +4259,7 @@ class Claim(Property):
                                            **kwargs)
         # TODO: Re-create the entire item from JSON, not just id
         self.snak = data['claim']['id']
+        self.on_item.latest_revision_id = data['pageinfo']['lastrevid']
 
     def getTarget(self):
         """
@@ -4338,10 +4339,10 @@ class Claim(Property):
         @type claims: list of pywikibot.Claim
         """
         data = self.repo.editSource(self, claims, new=True, **kwargs)
+        self.on_item.latest_revision_id = data['pageinfo']['lastrevid']
         source = defaultdict(list)
         for claim in claims:
             claim.hash = data['reference']['hash']
-            self.on_item.latest_revision_id = data['pageinfo']['lastrevid']
             source[claim.getID()].append(claim)
         self.sources.append(source)
 
@@ -4361,7 +4362,8 @@ class Claim(Property):
         @param sources: the sources to remove
         @type sources: list of pywikibot.Claim
         """
-        self.repo.removeSources(self, sources, **kwargs)
+        data = self.repo.removeSources(self, sources, **kwargs)
+        self.on_item.latest_revision_id = data['pageinfo']['lastrevid']
         for source in sources:
             source_dict = defaultdict(list)
             source_dict[source.getID()].append(source)
