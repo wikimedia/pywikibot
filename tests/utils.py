773e72eeafc0d5aa81442bafb9408db80277e841
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-09-25 20:28:05 +0000

    [FEAT] Test Python 3.5 on Travis
    As Python 3.5 has been officially released we should test on this. There are
    minor issues with deprecations as `inspect.getargspec` has been deprecated. But
    apart from a deprecation message it still works and will only be broken with
    Python 3.6.
    
    Bug: T106209
    Change-Id: Ic8256184d1c1dbf841def5ad3846155564e05b75

--- tests/utils.py
+++ tests/utils.py
@@ -227,6 +227,12 @@ class WarningSourceSkipContextManager(warnings.catch_warnings):
                     else:
                         skip_lines -= 1
 
+            # Avoid failures because getargspec hasn't been removed yet: T106209
+            if PYTHON_VERSION >= (3, 5, 0):
+                if str(entry.message) == ('inspect.getargspec() is deprecated, '
+                                          'use inspect.signature() instead'):
+                    return
+
             log.append(entry)
 
         log = super(WarningSourceSkipContextManager, self).__enter__()
