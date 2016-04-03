8a8a09f8dca94eb11057ad3e23fd3d40a938edee
Mpaa <mpaa.wiki@gmail.com>
2016-04-03 19:35:35 +0200

    Update archivebot_tests.py after MW localisation update
    Changed month name setting from Sep to sep
    See commit Localisation updates from https://translatewiki.net.
    I3d9b14ae3a5d77fea9694ef113b0180e5677c39e
    
    See file: languages/i18n/eo.json in mediawiki repo.
    
    Change-Id: I28ebec05941a4293d0178df8cf077bd5a46848e8

--- tests/archivebot_tests.py
+++ tests/archivebot_tests.py
@@ -23,7 +23,7 @@ from tests.aspects import unittest, TestCase
 
 THREADS = {
     'als': 4, 'ar': 1, 'bar': 0, 'bg': 0, 'bjn': 1, 'bs': 0, 'ca': 5, 'ckb': 2,
-    'cs': 0, 'de': 1, 'en': 25, 'eo': 1, 'es': 13, 'fa': 2, 'fr': 25, 'frr': 2,
+    'cs': 0, 'de': 1, 'en': 25, 'eo': 2, 'es': 13, 'fa': 2, 'fr': 25, 'frr': 2,
     'hi': 0, 'hr': 2, 'hu': 5, 'id': 3, 'it': 25, 'ja': 4, 'la': 0, 'lt': 1,
     'nl': 9, 'nn': 0, 'no': 0, 'pdc': 25, 'pfl': 3, 'pl': 8, 'pt': 0, 'ro': 1,
     'ru': 20, 'scn': 2, 'simple': 1, 'sr': 0, 'sv': 5, 'th': 1, 'tr': 7,
@@ -78,9 +78,14 @@ class TestArchiveBot(TestCase):
                     pywikibot.output('code %s: %s' % (thread.code, thread.content))
                 raise
 
-    expected_failures = ['ar', 'pdc', 'th']
+    expected_failures = ['ar', 'eo', 'pdc', 'th']
     # FIXME:
     # 'ar': Uses Arabic acronym for TZ
+    # 'eo': changed month name setting in wiki from Sep to sep
+    #       Localisation updates from https://translatewiki.net.
+    #       Change-Id: I3d9b14ae3a5d77fea9694ef113b0180e5677c39e
+    #       ref: mediawiki languages/i18n/eo.json
+    #       for new entries it should work
     # 'pdc': changed month name setting in wiki over time (?)
     #   in old posts in talk page, February is "Feb.", site message gives
     #   <message name="feb" xml:space="preserve">Han.</message>.
