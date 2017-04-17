33903d60e1938cbf9178b1afe1ec62592bc4e59d
xqt <info@gno.de>
2017-04-17 13:29:59 +0200

    Update version string
    Change-Id: Ib4a25e7dbfc754afd1183f4666e4e2be4bcab0db

--- pywikibot/family.py
+++ pywikibot/family.py
@@ -1263,7 +1263,7 @@ class Family(object):
         Use L{pywikibot.tools.MediaWikiVersion} to compare version strings.
         """
         # Here we return the latest mw release for downloading
-        return '1.28.0'
+        return '1.28.1'
 
     def force_version(self, code):
         """
