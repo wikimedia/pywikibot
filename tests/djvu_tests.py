461f747f272d8d8c99beadcf5c51f4fa0fa423e0
Mpaa <mpaa.wiki@gmail.com>
2017-05-15 22:30:33 +0200

    djvu.py: add replace_page() and delete_page()
    Add:
    -. replace_page() to replace a page with a white page.
    -. delete_page() to remove a page.
    
    Also added:
    -. __repr__()
    -. __str__()
    -. __unicode__()
    
    Added (limited) tests.
    
    Change-Id: I475df22d42a00dacaca5d55e05f6784f25e407fd

--- tests/djvu_tests.py
+++ tests/djvu_tests.py
@@ -10,14 +10,19 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import os
 import subprocess
 
 from pywikibot.tools.djvu import DjVuFile
+from pywikibot.tools import PY2
 
 from tests import join_data_path, create_path_func
 from tests.aspects import unittest, TestCase
 
 join_djvu_data_path = create_path_func(join_data_path, 'djvu')
+
+if not PY2:
+    unicode = str
 
 
 class TestDjVuFile(TestCase):
@@ -36,15 +41,42 @@ class TestDjVuFile(TestCase):
         """Setup tests."""
         super(TestDjVuFile, cls).setUpClass()
         try:
-            subprocess.Popen(['djvudump'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            dp = subprocess.Popen(['djvudump'],
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+            stdoutdata, stderrdata = dp.communicate()
         except OSError:
             raise unittest.SkipTest('djvulibre library not installed.')
+
+    def test_repr_method(self):
+        """Test __repr__() method."""
+        djvu = DjVuFile(self.file_djvu)
+        expected = "pywikibot.tools.djvu.DjVuFile('%s')" % self.file_djvu
+        if PY2:
+            expected = expected.encode('utf-8')
+        self.assertEqual(repr(djvu), expected)
+
+    def test_str_method(self):
+        """Test __str__() method."""
+        djvu = DjVuFile(self.file_djvu)
+        expected = "DjVuFile('%s')" % self.file_djvu
+        if PY2:
+            expected = expected.encode('utf-8')
+        self.assertEqual(str(djvu), expected)
+
+    def test_unicode_method(self):
+        """Test __unicode__() method."""
+        djvu = DjVuFile(self.file_djvu)
+        expected = "DjVuFile('%s')" % self.file_djvu
+        if PY2:
+            self.assertEqual(unicode(djvu), expected)
+        else:
+            self.assertEqual(djvu.__unicode__(), expected)
 
     def test_file_existance(self):
         """Test file existence checks."""
         djvu = DjVuFile(self.file_djvu)
-        self.assertEqual(self.file_djvu, djvu.file)
+        self.assertEqual(os.path.abspath(self.file_djvu), djvu.file)
         self.assertRaises(IOError, DjVuFile, self.file_djvu_not_existing)
 
     def test_number_of_images(self):
@@ -59,7 +91,7 @@ class TestDjVuFile(TestCase):
                          ('{myfile.djvu}', ('1092x221', 600)))
 
     def test_get_most_common_info(self):
-        """Test page number generator."""
+        """Test that most common (size, dpi) are returned."""
         djvu = DjVuFile(self.file_djvu)
         self.assertEqual(djvu.get_most_common_info(), ('1092x221', 600))
 
@@ -71,23 +103,33 @@ class TestDjVuFile(TestCase):
         self.assertFalse(djvu.has_text())
 
     def test_get_existing_page_number(self):
-        """Test if djvu file contains text."""
+        """Test text is returned for existing page number."""
         djvu = DjVuFile(self.file_djvu)
         self.assertTrue(djvu.has_text())
         txt = djvu.get_page(1)
         self.assertEqual(txt, self.test_txt)
 
     def test_get_not_existing_page_number(self):
-        """Test if djvu file contains text."""
+        """Test error is raised if djvu page number is out of range."""
         djvu = DjVuFile(self.file_djvu)
         self.assertTrue(djvu.has_text())
         self.assertRaises(ValueError, djvu.get_page, 100)
 
     def test_get_not_existing_page(self):
-        """Test if djvu file contains text."""
+        """Test error is raised if djvu file has no text."""
         djvu = DjVuFile(self.file_djvu_wo_text)
         self.assertFalse(djvu.has_text())
-        self.assertRaises(ValueError, djvu.get_page, 100)
+        self.assertRaises(ValueError, djvu.get_page, 1)
+
+    def test_whiten_not_existing_page_number(self):
+        """Test djvu page cannot be whitend if page number is out of range."""
+        djvu = DjVuFile(self.file_djvu)
+        self.assertRaises(ValueError, djvu.whiten_page, 100)
+
+    def test_delete_not_existing_page_number(self):
+        """Test djvu page cannot be deleted if page number is out of range."""
+        djvu = DjVuFile(self.file_djvu)
+        self.assertRaises(ValueError, djvu.delete_page, 100)
 
     def test_clear_cache(self):
         """Test if djvu file contains text."""
