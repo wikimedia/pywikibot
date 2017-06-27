magul/pywikibot-core

[bugfix] Add colon when replacing category and file weblinks
External links to wiki categories are replaced by internal ones without
a colon before link target. This behavior is wrong as those internal
links to categories without a preleading colon are used for a different
approach.

In addition the problem involves also links to files.

Bug: T127745
Change-Id: If07182a6476498ad71ced821000bf91df2516cd8

tests/cosmetic_changes_tests.py
@@ -180,6 +180,25 @@ class TestDryCosmeticChanges(TestCosmeticChanges):
         self.assertEqual(
             '[https://de.wikipedia.org/w/api.php API]',
             self.cct.fixSyntaxSave('[https://de.wikipedia.org/w/api.php|API]'))
+        self.assertEqual(
+            '[[:Category:Example]]\n'
+            '[[:Category:Example|Description]]\n'
+            '[[:File:Example.svg]]\n'
+            '[[:File:Example.svg|Description]]\n'
+            '[[:Category:Example]]\n'
+            '[[:Category:Example|Description]]\n'
+            '[[:File:Example.svg]]\n'
+            '[[:File:Example.svg|Description]]\n',
+            self.cct.fixSyntaxSave(
+                '[https://en.wikipedia.org/wiki/Category:Example]\n'
+                '[https://en.wikipedia.org/wiki/Category:Example Description]\n'
+                '[https://en.wikipedia.org/wiki/File:Example.svg]\n'
+                '[https://en.wikipedia.org/wiki/File:Example.svg Description]\n'
+                '[[https://en.wikipedia.org/wiki/Category:Example]]\n'
+                '[[https://en.wikipedia.org/wiki/Category:Example Description]]\n'
+                '[[https://en.wikipedia.org/wiki/File:Example.svg]]\n'
+                '[[https://en.wikipedia.org/wiki/File:Example.svg Description]]\n'
+            ))
 
     def test_fixHtml(self):
         """Test fixHtml method."""

wikimedia/pywikibot

tests/cosmetic_changes_tests.py
@@ -120,6 +120,10 @@ class TestDryCosmeticChanges(TestCosmeticChanges):
         # necessary as the fixer needs the article path to fix it
         self.cct.site._siteinfo._cache['general'] = (
             {'articlepath': '/wiki/$1'}, True)
+        self.cct.site._namespaces = {
+            6: ['Datei', 'File'],
+            14: ['Kategorie', 'Category'],
+        }
         self.assertEqual(
             '[[Example|Page]]\n[[Example|Page]]\n[[Example|Page]]\n'
             '[[Example]]\n[[Example]]\n[[Example]]\n'
@@ -180,6 +184,26 @@ class TestDryCosmeticChanges(TestCosmeticChanges):
         self.assertEqual(
             '[https://de.wikipedia.org/w/api.php API]',
             self.cct.fixSyntaxSave('[https://de.wikipedia.org/w/api.php|API]'))
+        self.assertEqual(
+            '[[:Kategorie:Example]]\n'
+            '[[:Category:Example|Description]]\n'
+            '[[:Datei:Example.svg]]\n'
+            '[[:File:Example.svg|Description]]\n'
+            '[[:Category:Example]]\n'
+            '[[:Kategorie:Example|Description]]\n'
+            '[[:File:Example.svg]]\n'
+            '[[:Datei:Example.svg|Description]]\n',
+            self.cct.fixSyntaxSave(
+                '[https://de.wikipedia.org/wiki/Kategorie:Example]\n'
+                '[https://de.wikipedia.org/wiki/Category:Example Description]\n'
+                '[https://de.wikipedia.org/wiki/Datei:Example.svg]\n'
+                '[https://de.wikipedia.org/wiki/File:Example.svg Description]\n'
+                '[[https://de.wikipedia.org/wiki/Category:Example]]\n'
+                '[[https://de.wikipedia.org/wiki/Kategorie:Example Description]]\n'
+                '[[https://de.wikipedia.org/wiki/File:Example.svg]]\n'
+                '[[https://de.wikipedia.org/wiki/Datei:Example.svg Description]]\n'
+            ))
+        del self.cct.site._namespaces
 
     def test_fixHtml(self):
         """Test fixHtml method."""