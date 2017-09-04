wikimedia/pywikibot

commit ecb4bd67393c158ab9e9406d567dea4208ffd9a8

APISite.logevents: Do not ignore the namespace if it is 0
Bug: T174875
Change-Id: Icd3ea445a08fb88c104663952c4d9afd2a520739

pywikibot/site.py
@@ -4544,7 +4544,7 @@ class APISite(BaseSite):
             legen.request["leend"] = end
         if reverse:
             legen.request["ledir"] = "newer"
-        if namespace:
+        if namespace is not None:
             legen.request["lenamespace"] = namespace
         if tag:
             # Supported in version 1.16+; earlier sites will cause APIError

commit 5cf7fb4275847ab39cafd0e974a3c0cba9c70213

APISite.logevents: Allow empty list as the namespace parameter
This fixes a regression introduced in ecb4bd67393c158ab9e9406d567dea4208ffd9a8
which was causing test_logpages_dump to fail because the default value for
`namespace` parameter is `[]`.

Change-Id: I9ae4cac160702b12b49ff258e02b0acf5cdbfbad

pywikibot/site.py
@@ -4544,7 +4544,7 @@ class APISite(BaseSite):
             legen.request["leend"] = end
         if reverse:
             legen.request["ledir"] = "newer"
-        if namespace is not None:
+        if namespace or namespace == 0:
             legen.request["lenamespace"] = namespace
         if tag:
             # Supported in version 1.16+; earlier sites will cause APIError