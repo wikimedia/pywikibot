1ddefd46e4cd9cc0f3813a664305d1b1d84a3234
Mpaa <mpaa.wiki@gmail.com>
2017-12-01 20:03:31 +0100

    proofreadpage_tests: fix test_page_gen_redlink
    Recreated pages on en.wikisource.org and renamed pages in file.
    
    Bug: T181697
    Change-Id: I41f6c0568b1fce41093afb9cddc58443c565a819

--- tests/proofreadpage_tests.py
+++ tests/proofreadpage_tests.py
@@ -651,11 +651,11 @@ class TestIndexPageMappingsRedlinks(IndexPageTestCase):
 
     cached = True
 
-    index_name = 'Index:Pywikibot test page 1'
-    page_names = ['Page:Pywikibot test page 1/1',
-                  'Page:Pywikibot test page 2/2',
+    index_name = 'Index:Pywikibot test page.djvu'
+    page_names = ['Page:Pywikibot test page.djvu/1',
+                  'Page:Pywikibot test page.djvu/2',
                   ]
-    missing_name = 'Page:Pywikibot test page 2/2'
+    missing_name = 'Page:Pywikibot test page.djvu/2'
 
     @classmethod
     def setUpClass(cls):
@@ -675,7 +675,6 @@ class TestIndexPageMappingsRedlinks(IndexPageTestCase):
             n = self.index.get_number(page)
             self.assertEqual(self.index.get_page(n), page)
 
-    @unittest.expectedFailure  # T181697
     def test_page_gen_redlink(self):
         """Test Index page generator with redlinks."""
         # Check start/end limits.
