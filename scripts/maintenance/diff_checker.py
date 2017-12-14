5df7b078a044798a15de875408ea11553762374c
dalba <dalba.wiki@gmail.com>
2017-12-14 21:19:36 +0330

    imagecopy.py: Improve the module's docstring
    Add copyright and license lines to diff_checker.py.
    
    Change-Id: Ibcc8a442ab3ac415db853c2cb7352b8727636962

--- scripts/maintenance/diff_checker.py
+++ scripts/maintenance/diff_checker.py
@@ -14,7 +14,11 @@ Todo: The following rules can be added in the future:
         __future__.unicode_literals. But keep older code unchanged.
 
 """
-
+#
+# (C) Pywikibot team, 2017
+#
+# Distributed under the terms of the MIT license.
+#
 
 from __future__ import absolute_import, unicode_literals
 
