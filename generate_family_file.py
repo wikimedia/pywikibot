85e3b92a893ac6b75caf8605c638c235ec732f88
xqt <info@gno.de>
2018-01-08 18:03:25 +0100

    [DOC] Cleanup doc string
    Change-Id: Ice48b587d60beed0eed8a08e25e720fd3950f35f

--- generate_family_file.py
+++ generate_family_file.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-This script generates a family file from a given URL.
-
-Hackish, etc. Regexps, yes. Sorry, jwz.
-"""
+"""This script generates a family file from a given URL."""
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 #
 # (C) Merlijn van Deen, 2010-2013
-# (C) Pywikibot team, 2010-2016
+# (C) Pywikibot team, 2010-2018
 #
 # Distributed under the terms of the MIT license
 #
