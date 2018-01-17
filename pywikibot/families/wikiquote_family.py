07212924f054b434233a0f90bb80a8d6f3662d0a
Framawiki <framawiki@tools.wmflabs.org>
2018-01-17 22:48:23 +0100

    Update families files
    Change-Id: I48b08ddc1efe49de8eba1564b6ecc5e61c627421

--- pywikibot/families/wikiquote_family.py
+++ pywikibot/families/wikiquote_family.py
@@ -34,9 +34,9 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
         self.languages_by_size = [
             'en', 'it', 'pl', 'ru', 'cs', 'fa', 'de', 'pt', 'es', 'uk', 'sk',
             'fr', 'bs', 'he', 'tr', 'fi', 'ca', 'lt', 'th', 'bg', 'sl', 'eo',
-            'hy', 'el', 'hr', 'nn', 'id', 'zh', 'hu', 'su', 'li', 'ar', 'az',
+            'hy', 'el', 'hr', 'nn', 'id', 'zh', 'ar', 'hu', 'su', 'li', 'az',
             'ko', 'nl', 'ja', 'gu', 'sv', 'sr', 'gl', 'ur', 'te', 'ta', 'cy',
-            'la', 'no', 'ml', 'vi', 'et', 'kn', 'ku', 'be', 'ro', 'eu', 'hi',
+            'la', 'no', 'vi', 'ml', 'et', 'kn', 'be', 'ku', 'ro', 'eu', 'hi',
             'ka', 'da', 'sa', 'is', 'sq', 'mr', 'br', 'af', 'uz', 'zh-min-nan',
             'wo', 'ky',
         ]
