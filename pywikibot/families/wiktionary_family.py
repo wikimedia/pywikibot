e6290494652171c1628ed1a7fd3c21341a736199
Framawiki <framawiki@tools.wmflabs.org>
2018-03-25 14:32:20 +0200

    Update families files
    Change-Id: I253985e215a42cb407db6f1a9f9397d65c0bafe2

--- pywikibot/families/wiktionary_family.py
+++ pywikibot/families/wiktionary_family.py
@@ -32,19 +32,19 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
     def __init__(self):
         """Initializer."""
         self.languages_by_size = [
-            'en', 'mg', 'fr', 'sh', 'ru', 'es', 'zh', 'de', 'nl', 'lt', 'sv',
-            'ku', 'pl', 'el', 'it', 'fi', 'ta', 'hu', 'tr', 'ca', 'ko', 'io',
+            'en', 'mg', 'fr', 'sh', 'ru', 'es', 'zh', 'de', 'nl', 'sv', 'lt',
+            'pl', 'ku', 'el', 'it', 'fi', 'ta', 'hu', 'tr', 'ca', 'ko', 'io',
             'kn', 'pt', 'hy', 'vi', 'sr', 'ja', 'chr', 'hi', 'th', 'ro', 'no',
-            'id', 'ml', 'et', 'uz', 'li', 'my', 'or', 'te', 'cs', 'fa', 'eo',
+            'id', 'ml', 'et', 'my', 'uz', 'li', 'or', 'te', 'cs', 'fa', 'eo',
             'ar', 'jv', 'az', 'eu', 'gl', 'oc', 'da', 'br', 'lo', 'uk', 'hr',
             'fj', 'tg', 'bg', 'simple', 'ps', 'sk', 'cy', 'vo', 'wa', 'is',
             'zh-min-nan', 'la', 'af', 'scn', 'ast', 'he', 'ky', 'tl', 'sw',
-            'fy', 'nn', 'lv', 'co', 'pa', 'pnb', 'mn', 'ka', 'nds', 'sl', 'sq',
+            'fy', 'nn', 'lv', 'pa', 'co', 'pnb', 'mn', 'ka', 'nds', 'sl', 'sq',
             'lb', 'bs', 'nah', 'sa', 'kk', 'bn', 'tk', 'km', 'sm', 'mk', 'hsb',
-            'be', 'ms', 'ur', 'ga', 'an', 'wo', 'vec', 'ang', 'tt', 'sd', 'gn',
-            'mr', 'so', 'mt', 'csb', 'ug', 'ie', 'gd', 'st', 'roa-rup', 'si',
+            'ur', 'be', 'ms', 'ga', 'an', 'wo', 'vec', 'ang', 'tt', 'sd', 'mt',
+            'gn', 'mr', 'ie', 'so', 'csb', 'ug', 'gd', 'st', 'roa-rup', 'si',
             'hif', 'ia', 'mi', 'ay', 'kl', 'fo', 'jbo', 'ln', 'zu', 'na', 'gu',
-            'gv', 'kw', 'rw', 'ne', 'ts', 'om', 'qu', 'su', 'ss', 'ha', 'iu',
+            'gv', 'kw', 'rw', 'ts', 'ne', 'om', 'qu', 'su', 'ss', 'ha', 'iu',
             'am', 'dv', 'tpi', 'yi', 'ti', 'sg', 'tn', 'ks',
         ]
 
