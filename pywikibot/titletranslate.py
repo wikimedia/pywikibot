b21ac3583d75a5372ca2430759cde588c3e60364
xqt <info@gno.de>
2018-05-04 13:21:56 +0200

    [IMPR] Simplify titletranslate.translate()
    - assert statement ensures that page is given if site is None
    - predefine hints as a tuple which could be iterated by the for statement.
      The if could be omitted then.
    - use partition() instead of if .. then .. else to get codes and newname
    - use isdigit() instead of try .. except to calculate the codes and keep
      the code flat.
    
    Change-Id: I61a219ccbe66142151304c8477e9d808664ad31c

--- pywikibot/titletranslate.py
+++ pywikibot/titletranslate.py
@@ -19,7 +19,7 @@ from pywikibot.tools import deprecated_args
 
 
 @deprecated_args(family=None)
-def translate(page=None, hints=None, auto=True, removebrackets=False,
+def translate(page=None, hints=(), auto=True, removebrackets=False,
               site=None):
     """
     Return a list of links to pages on other sites based on hints.
@@ -30,58 +30,48 @@ def translate(page=None, hints=None, auto=True, removebrackets=False,
     brackets and the text between them is removed from the page title.
     If 'auto' is true, known year and date page titles are autotranslated
     to all known target languages and inserted into the list.
-
     """
     result = set()
 
     assert page or site
 
-    if site is None and page:
+    if site is None:
         site = page.site
 
-    if hints:
-        for h in hints:
-            if ':' not in h:
-                # argument given as -hint:xy where xy is a language code
-                codes = h
-                newname = ''
-            else:
-                codes, newname = h.split(':', 1)
-            if newname == '':
-                # if given as -hint:xy or -hint:xy:, assume that there should
-                # be a page in language xy with the same title as the page
-                # we're currently working on ...
-                if page is None:
-                    continue
-                newname = page.title(withNamespace=False)
-                # ... unless we do want brackets
-                if removebrackets:
-                    newname = re.sub(re.compile(r"\W*?\(.*?\)\W*?",
-                                                re.UNICODE), u" ", newname)
-            try:
-                number = int(codes)
-                codes = site.family.languages_by_size[:number]
-            except ValueError:
-                if codes == 'all':
-                    codes = site.family.languages_by_size
-                elif codes in site.family.language_groups:
-                    codes = site.family.language_groups[codes]
-                else:
-                    codes = codes.split(',')
+    for h in hints:
+        # argument may be given as -hint:xy where xy is a language code
+        codes, _, newname = h.partition(':')
+        if not newname:
+            # if given as -hint:xy or -hint:xy:, assume that there should
+            # be a page in language xy with the same title as the page
+            # we're currently working on ...
+            if page is None:
+                continue
+            newname = page.title(withNamespace=False)
+            # ... unless we do want brackets
+            if removebrackets:
+                newname = re.sub(re.compile(r'\W*?\(.*?\)\W*?',
+                                            re.UNICODE), ' ', newname)
+        if codes.isdigit():
+            codes = site.family.languages_by_size[:int(codes)]
+        elif codes == 'all':
+            codes = site.family.languages_by_size
+        elif codes in site.family.language_groups:
+            codes = site.family.language_groups[codes]
+        else:
+            codes = codes.split(',')
 
-            for newcode in codes:
-
-                if newcode in site.languages():
-                    if newcode != site.code:
-                        ns = page.namespace() if page else 0
-                        x = pywikibot.Link(newname,
-                                           site.getSite(code=newcode),
-                                           defaultNamespace=ns)
-                        result.add(x)
-                else:
-                    if config.verbose_output:
-                        pywikibot.output(u"Ignoring unknown language code %s"
-                                         % newcode)
+        for newcode in codes:
+            if newcode in site.languages():
+                if newcode != site.code:
+                    ns = page.namespace() if page else 0
+                    x = pywikibot.Link(newname,
+                                       site.getSite(code=newcode),
+                                       defaultNamespace=ns)
+                    result.add(x)
+            elif config.verbose_output:
+                pywikibot.output('Ignoring unknown language code {}'
+                                 .format(newcode))
 
     # Autotranslate dates into all other languages, the rest will come from
     # existing interwiki links.
