a511544c0633d33a50ad36978fd1dedf85a91121
xqt <info@gno.de>
2018-05-17 13:21:31 +0200

    [tests] Add py3.7 to travis test matrix
    Add Python 3.7 to pypi classifiers
    
    Pre-test was successfull:
    https://travis-ci.org/wikimedia/pywikibot/builds/488217964
    
    Change-Id: Iace76e8320d8bf717706817961cc102ccc01adb6

--- setup.py
+++ setup.py
@@ -262,6 +262,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki',
         'Topic :: Software Development :: Libraries :: Python Modules',
