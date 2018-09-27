wikimedia/pywikibot

commit b686ea35fea2943c9761e76cddc4d6b66aebaea3

[bugfix] re-enable notifications
- "text" value for notformat parameter is no longer available and there is
  not hint when this was dropped. Use 'special' instead.
- "format" keyword may also be None which does not return the content values.
- "prop" parameter is "list" by default.

Bug: T205184
Change-Id: Id05ee852b6f8ce0ad5aeb842f9d5a1f8e033b7e3

pywikibot/site.py
@@ -2321,16 +2321,23 @@ class APISite(BaseSite):
 
     @need_extension('Echo')
     def notifications(self, **kwargs):
-        """Yield Notification objects from the Echo extension."""
+        """Yield Notification objects from the Echo extension.
+
+        keyword format: If specified, notifications will be returned formatted
+            this way. Its value is either 'model', 'special' or None. Default
+            is 'special'.
+        type format: str or None
+
+        Refer API reference for other keywords.
+        """
         params = {
             'action': 'query',
             'meta': 'notifications',
-            'notprop': 'list',
-            'notformat': 'text',
+            'notformat': 'special',
         }
 
-        for key in kwargs:
-            params['not' + key] = kwargs[key]
+        for key, value in kwargs.items():
+            params['not' + key] = value
 
         data = self._simple_request(**params).submit()
         notifications = data['query']['notifications']['list']

commit a142a7ebaaa65af121e3cf0a8b175e391f0e4046

[bugfix] Fix doc errors introduced in b686ea35fea2 and e6ed12025dc1
+ resolve some other older doc errors

Bug: T187009
Change-Id: Icb69204554af276f28c574f5a4f86b4b55cd4223

pywikibot/data/api.py
@@ -1579,7 +1579,7 @@ class Request(MutableMapping):
         @param value: param value(s)
 
         @type value: unicode or str in site encoding
-            (string types may be a |-separated list)
+            (string types may be a `|`-separated list)
             iterable, where items are converted to unicode
             with special handling for datetime.datetime to convert it to a
             string using the ISO 8601 format accepted by the MediaWiki API.

pywikibot/site.py
@@ -2323,10 +2323,10 @@ class APISite(BaseSite):
     def notifications(self, **kwargs):
         """Yield Notification objects from the Echo extension.
 
-        keyword format: If specified, notifications will be returned formatted
+        @keyword format: If specified, notifications will be returned formatted
             this way. Its value is either 'model', 'special' or None. Default
             is 'special'.
-        type format: str or None
+        @type format: str or None
 
         Refer API reference for other keywords.
         """

scripts/archive/featured.py
@@ -3,54 +3,54 @@
 """
 Manage featured/good article/list status template.
 
-This script understands various command-line arguments:
+*** This script understands various command-line arguments: ***
 
- Task commands:
+Task commands:
 
--featured         use this script for featured articles. Default task if no task
-                  command is specified
+ -featured         use this script for featured articles. Default task
+                   if no task command is specified
 
--good             use this script for good articles.
+ -good             use this script for good articles.
 
--lists            use this script for featured lists.
+ -lists            use this script for featured lists.
 
--former           use this script for removing {{Link FA|xx}} from former
-                  fearured articles
+ -former           use this script for removing {{Link FA|xx}} from former
+                   fearured articles
 
-                  NOTE: you may have all of these commands in one run
+                   NOTE: you may have all of these commands in one run
 
- Option commands:
+Option commands:
 
--interactive:     ask before changing each page
+ -interactive:     ask before changing each page
 
--nocache          doesn't include cache files file to remember if the article
-                  already was verified.
+ -nocache          doesn't include cache files file to remember if the article
+                   already was verified.
 
--nocache:xx,yy    you may ignore language codes xx,yy,... from cache file
+ -nocache:xx,yy    you may ignore language codes xx,yy,... from cache file
 
--fromlang:xx,yy   xx,yy,zz,.. are the languages to be verified.
--fromlang:ar--fi  Another possible with range the languages
+ -fromlang:xx,yy   xx,yy,zz,.. are the languages to be verified.
+ -fromlang:ar--fi  Another possible with range the languages
 
--fromall          to verify all languages.
+ -fromall          to verify all languages.
 
--tolang:xx,yy     xx,yy,zz,.. are the languages to be updated
+ -tolang:xx,yy     xx,yy,zz,.. are the languages to be updated
 
--after:zzzz       process pages after and including page zzzz
-                  (sorry, not implemented yet)
+ -after:zzzz       process pages after and including page zzzz
+                   (sorry, not implemented yet)
 
--side             use -side if you want to move all {{Link FA|lang}} next to the
-                  corresponding interwiki links. Default is placing
-                  {{Link FA|lang}} on top of the interwiki links.
-                  (This option is deprecated with wikidata)
+ -side             use -side if you want to move all {{Link FA|lang}} next
+                   to the corresponding interwiki links. Default is placing
+                   {{Link FA|lang}} on top of the interwiki links.
+                   (This option is deprecated with wikidata)
 
--count            Only counts how many featured/good articles exist
-                  on all wikis (given with the "-fromlang" argument) or
-                  on several language(s) (when using the "-fromall" argument).
-                  Example: python pwb.py featured -fromlang:en,he -count
-                  counts how many featured articles exist in the en and he
-                  wikipedias.
+ -count            Only counts how many featured/good articles exist
+                   on all wikis (given with the "-fromlang" argument) or
+                   on several language(s) (when using the "-fromall" argument).
+                   Example: python pwb.py featured -fromlang:en,he -count
+                   counts how many featured articles exist in the en and he
+                   wikipedias.
 
--quiet            no corresponding pages are displayed.
+ -quiet            no corresponding pages are displayed.
 
 """
 #

scripts/patrol.py
@@ -23,8 +23,7 @@ be used then.
 On Wikisource, it'll also check if the page is on the author namespace in which
 case it'll also patrol pages which are linked from that page.
 
-An example can be found at:
-
+An example can be found at
 https://en.wikisource.org/wiki/User:Wikisource-bot/patrol_whitelist
 
 Commandline parameters

scripts/solve_disambiguation.py
@@ -705,11 +705,11 @@ class DisambiguationRobot(SingleSiteBot):
 
     @staticmethod
     def firstlinks(page):
-        """Return a list of first links of every line beginning with *.
+        """Return a list of first links of every line beginning with `*`.
 
         When a disambpage is full of unnecessary links, this may be useful
         to sort out the relevant links. E.g. from line
-        *[[Jim Smith (smith)|Jim Smith]] ([[1832]]-[[1932]]) [[English]]
+        `* [[Jim Smith (smith)|Jim Smith]] ([[1832]]-[[1932]]) [[English]]`
         it returns only 'Jim Smith (smith)'
         Lines without an asterisk at the beginning will be disregarded.
         No check for page existence, it has already been done.

scripts/weblinkchecker.py
@@ -642,7 +642,7 @@ class History(object):
     We assume that the first element in the list represents the first time we
     found this dead link, and the last element represents the last time.
 
-    Example:
+    Example::
 
      dict = {
          'https://www.example.org/page': [

scripts/welcome.py
@@ -84,7 +84,7 @@ This script understands the following command-line arguments:
 
 ********************************* GUIDE ***********************************
 
-Report, Bad and white list guide:
+*** Report, Bad and white list guide: ***
 
 1)  Set in the code which page it will use to load the badword, the
     whitelist and the report
@@ -118,7 +118,7 @@ Report, Bad and white list guide:
             * Search if he's in the report page, if yes he will be skipped
             * If no, he will be reported.
 
-Random signature guide:
+*** Random signature guide: ***
 
 Some welcomed users will answer to the one who has signed the welcome message.
 When you welcome many new users, you might be overwhelmed with such answers.

tests/archivebot_tests.py
@@ -241,7 +241,7 @@ class TestDiscussionPageObject(TestCase):
     def testTwoThreadsWithCommentedOutThread(self):
         """Test recognizing two threads and ignoring a commented out thread.
 
-        Talk:For-pywikibot-archivebot must have:
+        Talk:For-pywikibot-archivebot must have::
 
          {{User:MiszaBot/config
          |archive = Talk:Main_Page/archive

tests/plural_tests.py
@@ -48,7 +48,8 @@ class MetaPluralRulesTest(MetaTestCaseClass):
                 return test_static_rule
 
         for lang, rule in plural.plural_rules.items():
-            cls.add_method(dct, 'test_{0}'.format(lang), create_test(rule),
+            cls.add_method(dct, 'test_{0}'.format(lang.replace('-', '_')),
+                           create_test(rule),
                            doc_suffix='for "{0}"'.format(lang))
         return super(MetaPluralRulesTest, cls).__new__(cls, name, bases, dct)
 