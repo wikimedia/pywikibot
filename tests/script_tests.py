b8fdb12ca532bbb8a11f9507b3dc8d3ff79b8b68
xqt <info@gno.de>
2018-09-28 13:32:33 +0200

    [doc] Add a common help string to flickrripper.py
    - remove usage() function
    - move comments to related functions
    - show error messages and warnings when needed
    
    Bug: T199094
    Change-Id: I7bd590f3226cd3eef1fdb915af33a1b122528aa5

--- tests/script_tests.py
+++ tests/script_tests.py
@@ -376,6 +376,7 @@ class TestScriptHelp(PwbTestCase):
 
     _expected_failures = failed_dep_script_set
     # -help tests may pass even when packages are required
+    _expected_failures.discard('flickrripper')
     _expected_failures.discard('imageharvest')
     _expected_failures.discard('isbn')
     _expected_failures.discard('weblinkchecker')
@@ -402,7 +403,6 @@ class TestScriptSimulate(DefaultSiteTestCase, PwbTestCase):
 
     _expected_failures = {
         'catall',          # stdout user interaction
-        'flickrripper',    # Requires a flickr api key
         'upload',          # raises custom ValueError
     }.union(failed_dep_script_set)
 
