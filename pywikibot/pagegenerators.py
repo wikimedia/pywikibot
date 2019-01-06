3a97ee5f199e2ff9fa07613d503863bdf22c8816
Mpaa <mpaa.wiki@gmail.com>
2019-01-06 12:49:21 +0100

    api.py: check if QueryGenerator support namespaces (updated)
    Check if 'query+module' supports namespace parameter and set
    namespace filtering accordingly in pagagenerators.
    
    support_namespace() has been added for this purpose.
    
    Note:
    - issue FutureWarning to alert that set_namespace() will soon raise
      a TypeError (and support_namespace() will then be removed).
    - this will be a breaking change, so time is given to fix 3rd party-code.
    - TODO left, so it will also be clear future evolution.
    
    set_namespace() now returns a bool (it makes tests easier).
    This should not be an issue as now nothing is returned anyhow.
    
    Tests added for set_namespace() and support_namespace().
    
    Bug: T198452
    Change-Id: Ia2c548cb5a6a8311c4bb92d3d345d6f16df6f7b1

--- pywikibot/pagegenerators.py
+++ pywikibot/pagegenerators.py
@@ -14,7 +14,7 @@ These parameters are supported to specify which pages titles to print:
 &params;
 """
 #
-# (C) Pywikibot team, 2008-2018
+# (C) Pywikibot team, 2008-2019
 #
 # Distributed under the terms of the MIT license.
 #
@@ -503,16 +503,19 @@ class GeneratorFactory(object):
             self.gens.insert(0, gen)
 
         for i in range(len(self.gens)):
-            if isinstance(self.gens[i], pywikibot.data.api.QueryGenerator):
-                if self.namespaces:
+            if self.namespaces:
+                if (isinstance(self.gens[i], pywikibot.data.api.QueryGenerator)
+                        and self.gens[i].support_namespace()):
                     self.gens[i].set_namespace(self.namespaces)
-                if self.limit:
-                    self.gens[i].set_maximum_items(self.limit)
-            else:
-                if self.namespaces:
+                # QueryGenerator does not support namespace param.
+                else:
                     self.gens[i] = NamespaceFilterPageGenerator(
                         self.gens[i], self.namespaces, self.site)
-                if self.limit:
+
+            if self.limit:
+                try:
+                    self.gens[i].set_maximum_items(self.limit)
+                except AttributeError:
                     self.gens[i] = itertools.islice(self.gens[i], self.limit)
         if len(self.gens) == 0:
             if (self.titlefilter_list
@@ -1013,8 +1016,7 @@ class GeneratorFactory(object):
 
     def _handle_unconnectedpages(self, value):
         """Handle `-unconnectedpages` argument."""
-        # T196619 don't use QueryGenerator due to namespace filtering
-        return (p for p in self.site.unconnected_pages(total=_int_none(value)))
+        return self.site.unconnected_pages(total=_int_none(value))
 
     def _handle_imagesused(self, value):
         """Handle `-imagesused` argument."""
