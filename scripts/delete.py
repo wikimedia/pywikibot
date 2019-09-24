6c6df6ff2acf26838859058a3e428879546e2cf3
Huji Lee <huji.huji@gmail.com>
2019-09-23 20:43:00 -0400

    Make sure the user is logged in before attempting a delete
    When BotPasswords is used, even if you have already run the login.py
    script, you will still end up logged out when you run the delete.py
    script later. This patch provides a workaround so that delete.py remains
    usable while we revamp the BotPasswords related processes to make the
    login session info persistant.
    
    Bug: T233608
    Change-Id: Ib3bc9005ab58ff19ce14acf9df4127a7d32eb746

--- scripts/delete.py
+++ scripts/delete.py
@@ -214,6 +214,8 @@ class DeletionRobot(MultipleSitesBot, CurrentPageBot):
                             self.current_page, ns_names))
                     return  # Not an orphan, do not delete.
 
+            if self.site.user() is None:
+                self.site.login()
             self.current_page.delete(self.summary,
                                      not self.getOption('always'),
                                      self.getOption('always'),
