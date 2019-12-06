mariaph83/pywikibotGerritMirror

component: fix docstrings to avoid warnings
Use fully specified type names to avoid many warnings
when building documentation.

Bug: T187009
Change-Id: I352c3aab964446777b5bf8142422b2efc65ec467

pywikibot/family.py
@@ -986,7 +986,7 @@ class Family(object):
         @param fam: family name (if omitted, uses the configured default)
         @type fam: str
         @return: a Family instance configured for the named family.
-        @raises UnknownFamily: family not known
+        @raises pywikibo.exceptions.UnknownFamily: family not known
         """
         if fam is None:
             fam = config.family

wikimedia/pywikibot

pywikibot/family.py
@@ -986,7 +986,7 @@ class Family(object):
         @param fam: family name (if omitted, uses the configured default)
         @type fam: str
         @return: a Family instance configured for the named family.
-        @raises UnknownFamily: family not known
+        @raises pywikibot.exceptions.UnknownFamily: family not known
         """
         if fam is None:
             fam = config.family