d75efc5fda8badd9f3451d9c196746c9eacce363
xqt <info@gno.de>
2019-12-15 14:31:46 +0100

    [setup] Add python 3.8 to the setup classifier
    Change-Id: Ieb3fb179022871f0872b3e6a77f1a8d74e5f2dac

--- setup.py
+++ setup.py
@@ -237,6 +237,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Wiki',
         'Topic :: Software Development :: Libraries :: Python Modules',
