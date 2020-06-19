wikimedia/pywikibot

commit 9fa2deeef035b233b3cc2babc8193e299cd129f2

[L10N] Add welcome tempates for several ml projects
Bug: T255744
Change-Id: I69d9fcd346b1dfc0883bfcf1bb795cf46e7c08aa

scripts/welcome.py
@@ -157,7 +157,7 @@ Some words, like "Administrator" or "Dio" (God in italian) or "Jimbo" aren't
 badwords at all but can be used for some bad-nickname.
 """
 #
-# (C) Pywikibot team, 2006-2019
+# (C) Pywikibot team, 2006-2020
 #
 # Distributed under the terms of the MIT license.
 #
@@ -262,6 +262,7 @@ netext = {
         'ar': '{{subst:ترحيب}} %s',
         'bs': '{{Dobrodošlica}} %s',
         'es': '{{subst:bienivenido usuario}} %s',
+        'ml': '{{subst:സ്വാഗതം}}',
     },
     'wikinews': {
         'ar': '{{subst:ترحيب}} %s',
@@ -270,11 +271,15 @@ netext = {
         'it': '{{subst:benvenuto}}',
         'zh': '{{subst:welcome}} %s',
     },
+    'wikiquote': {
+        'ml': '{{subst:സ്വാഗതം}}',
+    },
     'wikisource': {
         'ar': '{{subst:ترحيب}} %s',
         'bn': '{{subst:স্বাগতম}} %s',
         'bs': '{{Dobrodošlica}} %s',
         'kn': '{{subst:ಸುಸ್ವಾಗತ}} %s',
+        'ml': '{{subst:സ്വാഗതം}}',
         'mr': '{{subst:Welcome}} %s',
     },
     'wiktionary': {
@@ -284,6 +289,7 @@ netext = {
         'fa': '{{جا:خوشامد|%s}}',
         'kn': '{{subst:ಸುಸ್ವಾಗತ}} %s',
         'it': '{{subst:Utente:Filnik/Benve|firma=%s}}',
+        'ml': '{{subst:സ്വാഗതം}}',
         'ur': '{{جا:خوش آمدید}}%s',
     },
     'wikiversity': {

commit 3edcc245d0de5a1fc21f42c1e710c85aaab91b61

[bugfix] Fix regression introduced in 9fa2deee
After the patch, wikiquotes fallback to wikipedia has started to
be broken. This should be solved more generally, but until then
this quick fix should do. We could also copy the whole wikipedia
into wikiquotes, but I don't like that solution much.

Change-Id: I8a6f751edd1cd3067c35fa56b3a7be631bb942b6

scripts/welcome.py
@@ -272,6 +272,7 @@ netext = {
         'zh': '{{subst:welcome}} %s',
     },
     'wikiquote': {
+        'ar': '{{subst:ترحيب}} %s',
         'ml': '{{subst:സ്വാഗതം}}',
     },
     'wikisource': {