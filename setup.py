e7eb5d823cdf17fcbf862d6edbe324d565575f4a
xqt <info@gno.de>
2020-08-04 17:40:47 +0200

    [doc] Add Python 3 only classifier
    Change-Id: I6d8c0a22bb4a3df0c2b4a8cc8ff5549db31f4024

--- setup.py
+++ setup.py
@@ -270,6 +270,7 @@ def main():
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
