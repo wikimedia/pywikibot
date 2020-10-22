19c7c8cf7c1b920a1d28379d9874045d01150697
Tigerfell <tigerfell-688@tuta.io>
2020-10-22 23:50:02 +0200

    Update OpenStreetMap family
    Publish additional wiki-specific settings I have set to operate a bot on
    https://wiki.openstreetmap.org, the change appends the list of settings by
    some names of templates, categories, and pages.
    
    Change-Id: I69737a08e1bc932db32806a4f3a01aad6d91d7ea

--- pywikibot/families/osm_family.py
+++ pywikibot/families/osm_family.py
@@ -17,6 +17,34 @@ class Family(family.SingleSiteFamily):
     domain = 'wiki.openstreetmap.org'
     code = 'en'
 
+    # Templates that indicate a category redirect
+    # Redirects to these templates are automatically included
+    category_redirect_templates = {
+        'en': ('Category redirect',),
+    }
+
+    # A list of disambiguation template names in different languages
+    disambiguationTemplates = {  # noqa: N815
+        'en': ('Disambig',),
+    }
+
+    # A dict with the name of the category containing disambiguation
+    # pages for the various languages. Only one category per language,
+    # and without the namespace
+    disambcatname = {
+        'en': 'Disambiguation',
+    }
+
+    # Subpages for documentation
+    doc_subpages = {
+        'en': ('/doc',),
+    }
+
+    # Templates that indicate an edit should be avoided
+    edit_restricted_templates = {
+        'en': ('In Bearbeitung',),
+    }
+
     def protocol(self, code):
         """Return https as the protocol for this family."""
         return 'https'
