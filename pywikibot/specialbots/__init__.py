2bb702ebb454661a01e91f76523fb3cb969ccea4
shubham656 <shubham656jain@gmail.com>
2020-11-02 00:11:49 +0530

    Modify __init__.py to support python 3.5+ only
    Bug: T257399
    Change-Id: I0dd5126d3220c19733210706bbdb3f7343ef2d03

--- pywikibot/specialbots/__init__.py
+++ pywikibot/specialbots/__init__.py
@@ -6,8 +6,6 @@
 #
 # Distributed under the terms of the MIT license.
 #
-from __future__ import absolute_import, division, unicode_literals
-
 from pywikibot.specialbots._unlink import (
     EditReplacement, InteractiveUnlink, BaseUnlinkBot
 )
