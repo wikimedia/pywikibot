f6d36f9f013141e7930c6e84971b36d31c1fce67
xqt <info@gno.de>
2020-12-15 17:31:01 +0100

    [L10N] L10N updates
    - update languages_by_size
    - update linktrails
    
    Bug: T269442
    Bug: T269434
    Bug: T269429
    Bug: T268460
    Bug: T268414
    Change-Id: I36ed90e2cdff6d1ed1b494e4015b05d6ef9df671

--- pywikibot/families/wiktionary_family.py
+++ pywikibot/families/wiktionary_family.py
@@ -31,18 +31,18 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
     languages_by_size = [
         'en', 'fr', 'mg', 'ru', 'de', 'sh', 'es', 'zh', 'el', 'nl', 'sv', 'pl',
         'ku', 'lt', 'it', 'ca', 'fi', 'ta', 'hu', 'tr', 'io', 'hy', 'ko', 'kn',
-        'pt', 'ja', 'vi', 'sr', 'th', 'chr', 'hi', 'ro', 'no', 'et', 'id',
+        'pt', 'ja', 'vi', 'sr', 'th', 'chr', 'hi', 'ro', 'no', 'id', 'et',
         'ml', 'cs', 'my', 'uz', 'li', 'or', 'te', 'eo', 'fa', 'ar', 'gl', 'jv',
         'oc', 'az', 'eu', 'uk', 'br', 'da', 'lo', 'ast', 'hr', 'is', 'simple',
         'la', 'fj', 'tg', 'ky', 'sk', 'bg', 'wa', 'ur', 'ps', 'cy', 'vo', 'af',
-        'zh-min-nan', 'he', 'shn', 'scn', 'tl', 'pa', 'sw', 'fy', 'sl', 'bn',
+        'zh-min-nan', 'he', 'shn', 'scn', 'tl', 'pa', 'sw', 'sl', 'fy', 'bn',
         'nn', 'lv', 'ka', 'sq', 'co', 'mn', 'pnb', 'min', 'lb', 'nds', 'bs',
         'nah', 'sa', 'ms', 'kk', 'yue', 'km', 'vec', 'tk', 'mk', 'be', 'sm',
         'hsb', 'shy', 'gd', 'ga', 'an', 'wo', 'gom', 'ang', 'ia', 'tt', 'mt',
         'sd', 'gn', 'mr', 'fo', 'ie', 'so', 'csb', 'ug', 'st', 'roa-rup', 'si',
         'hif', 'zu', 'kl', 'mi', 'su', 'ay', 'jbo', 'ln', 'yi', 'gu', 'na',
         'gv', 'kw', 'tpi', 'rw', 'ne', 'ts', 'om', 'qu', 'ha', 'ss', 'iu',
-        'am', 'dv', 'sg', 'ti', 'tn', 'ks',
+        'am', 'dv', 'sg', 'ti', 'tn', 'ks', 'skr',
     ]
 
     category_redirect_templates = {
