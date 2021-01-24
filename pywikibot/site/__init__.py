a0dca679d7000a23f9b5e83dff5042e5b0dd8c19
xqt <info@gno.de>
2021-01-24 11:34:20 +0100

    Revert "[cleanup] Deprecate loadimageinfo return statement"
    Python 6.0.6 may be a breaking change and this new behaviour
    is already announced. The deprecation warning does not work
    for Python 3.5.
    
    This reverts commit 0010abde8dfd37cd8d2bdead4dffa072cf378f8f.
    
    Change-Id: Id629c8485631789ca5f931e00998e4094ef21508

--- pywikibot/site/__init__.py
+++ pywikibot/site/__init__.py
@@ -12,11 +12,9 @@ groups of wikis on the same topic in different languages.
 import datetime
 import functools
 import heapq
-import inspect
 import itertools
 import json
 import mimetypes
-import opcode
 import os
 import re
 import time
@@ -1328,23 +1326,6 @@ class APISite(BaseSite):
                 raise PageRelatedError(
                     page,
                     'loadimageinfo: Query on %s returned no imageinfo')
-
-        # inspect whether the result is assigned and print an deprecation
-        # warning in that case; implementation inspired by
-        # https://stackoverflow.com/questions/813882/is-there-a-way-to-check-whether-function-output-is-assigned-to-a-variable-in-pyt
-        try:
-            frame = inspect.currentframe().f_back
-            next_opcode = opcode.opname[
-                frame.f_code.co_code[frame.f_lasti + 3]]
-            if next_opcode in ('POP_TOP', 'ROT_TWO', 'ROT_THREE'):
-                issue_deprecation_warning(
-                    'APISite.loadimageinfo() result dict and any assignment',
-                    "pageitem['imageinfo'] if history is True "
-                    "else pageitem['imageinfo'][0]",
-                    warning_class=FutureWarning,
-                    since='20210110')
-        finally:
-            del frame
 
         return (pageitem['imageinfo']
                 if history else pageitem['imageinfo'][0])
