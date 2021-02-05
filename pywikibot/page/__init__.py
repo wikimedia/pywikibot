ba5ad1f19df7855429852c428977b2fc7a481340
xqt <info@gno.de>
2021-02-05 11:26:33 +0100

    [IMPR ]Move page decorators into _decorators.py file
    page.py is one of the biggest framework file with 236 KB disk
    space and 5100 lines of code. This should be splitted into smaller
    parts for readability and maintainability. Moving decorators to its
    own file like in site module.
    
    Change-Id: I7b633791456160df0eae1cb48bd4ccf88842c2d5

--- pywikibot/page/__init__.py
+++ pywikibot/page/__init__.py
@@ -43,10 +43,10 @@ from pywikibot.exceptions import (
     UserRightsError,
 )
 from pywikibot.family import Family
+from pywikibot.page._decorators import allow_asynchronous
 from pywikibot.page._revision import Revision
 from pywikibot.site import DataSite, Namespace
 from pywikibot.tools import (
-    add_full_name,
     compute_file_hash,
     ComparableMixin,
     deprecated,
@@ -55,7 +55,6 @@ from pywikibot.tools import (
     DotReadableDict,
     first_upper,
     issue_deprecation_warning,
-    manage_wrapping,
     redirect_func,
     remove_last_args,
 )
@@ -88,49 +87,6 @@ __all__ = (
 )
 
 logger = logging.getLogger('pywiki.wiki.page')
-
-
-@add_full_name
-def allow_asynchronous(func):
-    """
-    Decorator to make it possible to run a BasePage method asynchronously.
-
-    This is done when the method is called with kwarg asynchronous=True.
-    Optionally, you can also provide kwarg callback, which, if provided, is
-    a callable that gets the page as the first and a possible exception that
-    occurred during saving in the second thread or None as the second argument.
-    """
-    def handle(func, self, *args, **kwargs):
-        do_async = kwargs.pop('asynchronous', False)
-        callback = kwargs.pop('callback', None)
-        err = None
-        try:
-            func(self, *args, **kwargs)
-        # TODO: other "expected" error types to catch?
-        except pywikibot.Error as edit_err:
-            err = edit_err  # edit_err will be deleted in the end of the scope
-            link = self.title(as_link=True)
-            if do_async:
-                pywikibot.error('page {} not saved due to {}\n'
-                                .format(link, err))
-            pywikibot.log('Error saving page %s (%s)\n' % (link, err),
-                          exc_info=True)
-            if not callback and not do_async:
-                if isinstance(err, pywikibot.PageSaveRelatedError):
-                    raise err
-                raise pywikibot.OtherPageSaveError(self, err)
-        if callback:
-            callback(self, err)
-
-    def wrapper(self, *args, **kwargs):
-        if kwargs.get('asynchronous'):
-            pywikibot.async_request(handle, func, self, *args, **kwargs)
-        else:
-            handle(func, self, *args, **kwargs)
-
-    manage_wrapping(wrapper, func)
-
-    return wrapper
 
 
 # Note: Link objects (defined later on) represent a wiki-page's title, while
