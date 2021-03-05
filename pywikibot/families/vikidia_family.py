98a91bbb5d4ff07fd9174bb0b8776c059e7caa16
Linedwell <Linedwell@icloud.com>
2021-03-05 09:56:19 +0000

    Added pt.vikidia to Vikidia Family
    Change-Id: Ibe1745487c73bf684813c3bb83b4b167ec7d1a89

--- pywikibot/families/vikidia_family.py
+++ pywikibot/families/vikidia_family.py
@@ -1,6 +1,6 @@
 """Family module for Vikidia."""
 #
-# (C) Pywikibot team, 2010-2020
+# (C) Pywikibot team, 2010-2021
 #
 # Distributed under the terms of the MIT license.
 #
@@ -14,7 +14,10 @@ class Family(family.SubdomainFamily):
     name = 'vikidia'
     domain = 'vikidia.org'
 
-    codes = ['ca', 'de', 'el', 'en', 'es', 'eu', 'fr', 'hy', 'it', 'ru', 'scn']
+    codes = [
+        'ca', 'de', 'el', 'en', 'es', 'eu', 'fr', 'hy', 'it', 'pt', 'ru',
+        'scn',
+    ]
 
     # Sites we want to edit but not count as real languages
     test_codes = ['central', 'test']
