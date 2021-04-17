c1141e7e64fb424ff7aa134cec9d304be68042af
xqt <info@gno.de>
2021-04-17 08:04:18 +0200

    [doc] Add specialbots to doc
    Sphinx uses __all__ to include code parts into
    
    Bug: T280316
    Change-Id: I38eef18829506358bba2df835da811a7de2a744a

--- pywikibot/specialbots/__init__.py
+++ pywikibot/specialbots/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """Module containing special bots reusable by scripts."""
 #
-# (C) Pywikibot team, 2020
+# (C) Pywikibot team, 2020-2021
 #
 # Distributed under the terms of the MIT license.
 #
@@ -9,3 +9,11 @@ from pywikibot.specialbots._unlink import (
     EditReplacement, InteractiveUnlink, BaseUnlinkBot
 )
 from pywikibot.specialbots._upload import UploadRobot
+
+
+__all__ = (
+    'BaseUnlinkBot',
+    'EditReplacement',
+    'InteractiveUnlink',
+    'UploadRobot',
+)
