4dc8b7e868a8c20061d97722f504ab6cb8d3513c
xqt <info@gno.de>
2021-04-30 10:09:34 +0200

    [6.2] Bump version to 6.2.0
    Change-Id: I3b72d1690cf9d5ed98fd06572781a593840307a1

--- pywikibot/__metadata__.py
+++ pywikibot/__metadata__.py
@@ -8,7 +8,7 @@ from time import strftime
 
 
 __name__ = 'pywikibot'
-__version__ = '6.1.1.dev0'
+__version__ = '6.2.0.dev0'
 __description__ = 'Python MediaWiki Bot Framework'
 __maintainer__ = 'The Pywikibot team'
 __maintainer_email__ = 'pywikibot@lists.wikimedia.org'
