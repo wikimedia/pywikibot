wikimedia/pywikibot

commit fd605f04ed6d6bf9dc531ab52059a0cd5d80965c

[cleanup] Consistently use string's format()
String formatting from PEP3101 is intended to replace printf style
formatting [1][2]. date.py, i18n.py, and a few scripts have especially
sophisticated usage so leaving those be.

This also drops unnecessary index references [3] and repr() [4] from format
calls.

[1] https://www.python.org/dev/peps/pep-3101/#abstract
[2] https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting

[3] The following are identical...

    >>> '{0} {1}'.format('arg1', 'arg2')
    >>> '{} {}'.format('arg1', 'arg2')

[4] The following are identical...

    >>> 'repr value: {}.format(repr(my_arg))
    >>> 'repr value: {!r}.format(my_arg)

Change-Id: I5191814201ca1f4dcfa3a6c587be0dd2b9fe76ff

scripts/category_redirect.py
@@ -64,8 +64,8 @@ class CategoryRedirectBot(SingleSiteBot):
         self.template_list = []
         self.cat = None
         self.log_page = pywikibot.Page(self.site,
-                                       'User:%(user)s/category redirect log'
-                                       % {'user': self.site.username()})
+                                       'User:{}/category redirect log'
+                                       .format(self.site.username()))
 
         # Localization:
 
@@ -292,7 +292,7 @@ class CategoryRedirectBot(SingleSiteBot):
         self.newredirs = []
 
         localtime = time.localtime()
-        today = '%04d-%02d-%02d' % localtime[:3]
+        today = '{:04d}-{:02d}-{:02d}'.format(*localtime[:3])
         self.datafile = pywikibot.config.datafilepath(
             '{}-catmovebot-data'.format(self.site.dbName()))
         try:
@@ -473,8 +473,9 @@ class CategoryRedirectBot(SingleSiteBot):
         self.newredirs.sort()
         comment = i18n.twtranslate(self.site, self.maint_comment)
         message = i18n.twtranslate(self.site, 'category_redirect-log-new')
-        self.log_page.text = ('\n== %i-%02i-%02iT%02i:%02i:%02iZ ==\n'
-                              % time.gmtime()[:6]
+        date_line = '\n== {}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}Z ==\n' \
+                    .format(*time.gmtime()[:6])
+        self.log_page.text = (date_line,
                               + '\n'.join(self.log_text)
                               + '\n* ' + message + '\n'
                               + '\n'.join(self.newredirs)

commit 410581e045826b8a857f9edc4200a1a451a7451d

[bugfix] Fix unsupported object unary operand
Bug was introduced with fd605f0

Change-Id: Ie3ec6ffe7852af71bc0591c22d73bcae1ace0902

scripts/category_redirect.py
@@ -475,7 +475,7 @@ class CategoryRedirectBot(SingleSiteBot):
         message = i18n.twtranslate(self.site, 'category_redirect-log-new')
         date_line = '\n== {}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}Z ==\n' \
                     .format(*time.gmtime()[:6])
-        self.log_page.text = (date_line,
+        self.log_page.text = (date_line
                               + '\n'.join(self.log_text)
                               + '\n* ' + message + '\n'
                               + '\n'.join(self.newredirs)