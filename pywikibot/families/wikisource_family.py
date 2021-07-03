5647f5a0983d92741b2bc1fc8b548d956cd3d49a
Meno25 <meno25mail@gmail.com>
2021-07-03 19:31:08 +0000

    [L10N] Update languages_by_size
    * Also update Interwiki sorting order
    ** See https://meta.wikimedia.org/w/index.php?diff=21677344
    ** and https://meta.wikimedia.org/w/index.php?diff=21677335
    
    Change-Id: I3583d82c367376381665ff670f925a8f502b6457

--- pywikibot/families/wikisource_family.py
+++ pywikibot/families/wikisource_family.py
@@ -28,7 +28,7 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
         'pl', 'en', 'ru', 'de', 'fr', 'zh', 'he', 'it', 'es', 'ar', 'uk', 'cs',
         'gu', 'mul', 'pt', 'sr', 'fa', 'hu', 'sv', 'ko', 'ml', 'sa', 'bn',
         'te', 'ta', 'sl', 'hy', 'el', 'tr', 'ro', 'fi', 'ja', 'vi', 'nap',
-        'az', 'la', 'ca', 'br', 'nl', 'kn', 'hr', 'th', 'no', 'eo', 'is',
+        'la', 'az', 'ca', 'br', 'nl', 'kn', 'hr', 'th', 'no', 'eo', 'is',
         'vec', 'pms', 'id', 'hi', 'be', 'et', 'lij', 'da', 'mk', 'yi', 'as',
         'mr', 'bg', 'li', 'lt', 'wa', 'eu', 'or', 'pa', 'cy', 'gl', 'bs',
         'sah', 'sk', 'zh-min-nan', 'fo', 'ban',
