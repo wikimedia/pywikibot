66a40bcf9f6c48a826939bf52a96b1ee519bc1e1
xqt <info@gno.de>
2021-11-15 06:00:58 +0100

    [doc] fix doc in scripts/__init__.py
    Change-Id: I2849b53dcd04cccb1b4ed9588cd8b83476469095

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,7 +1,7 @@
 """**Scripts** folder contains predefined scripts easy to use.
 
-Scripts are only available im Pywikibot if instaled in directory mode
-and not as side package. They can be run in command line using the pwb
+Scripts are only available with Pywikibot if installed in directory mode
+and not as site package. They can be run in command line using the pwb
 wrapper script::
 
     python pwb.py <global options> <name_of_script> <options>
