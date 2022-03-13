bdd9db9daacdc40c8b7c772211a91e9633f92be0
Meno25 <meno25mail@gmail.com>
2022-03-13 14:01:31 +0000

    [L10N] Update languages_by_size
    Change-Id: I46f8f6ed9750110728ec50a146bb11b0f204837c

--- pywikibot/families/wikiquote_family.py
+++ pywikibot/families/wikiquote_family.py
@@ -27,11 +27,11 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
     ]
 
     languages_by_size = [
-        'it', 'en', 'pl', 'ru', 'cs', 'et', 'fa', 'pt', 'de', 'uk', 'fr', 'es',
-        'he', 'tr', 'sk', 'bs', 'eo', 'ca', 'az', 'fi', 'sl', 'lt', 'zh', 'ar',
+        'it', 'en', 'pl', 'ru', 'cs', 'et', 'fa', 'pt', 'de', 'uk', 'fr', 'he',
+        'es', 'tr', 'sk', 'bs', 'eo', 'ca', 'az', 'fi', 'sl', 'lt', 'zh', 'ar',
         'bg', 'hy', 'hr', 'el', 'su', 'nn', 'id', 'sv', 'li', 'hu', 'ko', 'nl',
         'ja', 'la', 'sah', 'ta', 'sr', 'gu', 'gl', 'ur', 'te', 'be', 'cy',
-        'no', 'vi', 'ml', 'sq', 'kn', 'ro', 'eu', 'ku', 'uz', 'hi', 'ka', 'da',
+        'no', 'vi', 'ml', 'sq', 'kn', 'eu', 'ro', 'ku', 'hi', 'uz', 'ka', 'da',
         'sa', 'is', 'th', 'br', 'mr', 'af', 'wo', 'ky',
     ]
 
