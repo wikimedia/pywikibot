98b293ed0baa83ad0845ac4f5ed9c3460abb5c63
xqt <info@gno.de>
2022-03-15 17:11:49 +0100

    [flake] remove E501 flake8 exception at wikisource
    Change-Id: I32bcb85f25a778fb1456473c8eaa98de92996d9c

--- pywikibot/families/wikisource_family.py
+++ pywikibot/families/wikisource_family.py
@@ -16,11 +16,11 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
     name = 'wikisource'
 
     closed_wikis = [
-        # https://noc.wikimedia.org/conf/highlight.php?file=dblists/closed.dblist  # noqa: E501
+        # https://noc.wikimedia.org/conf/highlight.php?file=dblists/closed.dblist
         'ang', 'ht',
     ]
     removed_wikis = [
-        # https://noc.wikimedia.org/conf/highlight.php?file=dblists/deleted.dblist  # noqa: E501
+        # https://noc.wikimedia.org/conf/highlight.php?file=dblists/deleted.dblist
         'tokipona',
     ]
 
