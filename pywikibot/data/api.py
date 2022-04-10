2a645071de33ba4c5d1bb5b0b8ba89892d48ef7b
Mpaa <mpaa.wiki@gmail.com>
2022-04-10 12:01:07 +0200

    [bugfix]: Do not delete text when updating a Revision
    Do not overwrite existing Revision if it does not add text.
    
    In tests:
    - reset cached revisions to keep test uniform for Page and ItemPage;
      Page and ItemPage have different side effects on page._revisions,
      due to BotMayEdit() implementations, and loadrevisions() does not
      necessarily reset Revision in cache.
    
    Bug: T304786
    Change-Id: I306291c50127594c6457e61e8d5b70daeead8557

--- pywikibot/data/api.py
+++ pywikibot/data/api.py
@@ -3117,7 +3117,13 @@ def _update_protection(page, pagedict: dict) -> None:
 def _update_revisions(page, revisions) -> None:
     """Update page revisions."""
     for rev in revisions:
-        page._revisions[rev['revid']] = pywikibot.page.Revision(**rev)
+        revid = rev['revid']
+        revision = pywikibot.page.Revision(**rev)
+        # do not overwrite an existing Revision if there is no content
+        if revid in page._revisions and revision.text is None:
+            pass
+        else:
+            page._revisions[revid] = revision
 
 
 def _update_templates(page, templates) -> None:
