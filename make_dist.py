e9d62d93b2a252458d158949e36d8489c0af6e5a
xqt <info@gno.de>
2022-05-14 17:16:52 +0200

    [IMPR] Add a new script to create a Pywikibot distribution
    - Add a new script "make_dist.py" to create a new distribution.
    - With -local option the distribution is installed as local site-package.
      It uninstalls an already installed Pywikibot site-package version.
    - With -remote option the distribution is uploaded to pypi if it is not
      a development release
    - Change setup.py to allow creating a development release package.
    - Update documentations
    - Add setup.py and make_dist.py to sphinx documentation
    - Exclude make_dist from flake83py35 tests because 3.6+ is needed for
      this script
    
    Change-Id: I06a830c5309b0416d90ff5206c268f5495ee7d02

---
+++ make_dist.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+"""Script to create a new distribution.
+
+The following options are supported:
+
+-help    Print documentation of this file and of setup.py
+
+-local   Install the distribution as a local site-package. If a
+         Pywikibot package is already there, it will be uninstalled
+         first.
+
+-remote  Upload the package to pypi. This cannot be done if the
+         Pywikibot version is a development release.
+
+Usage::
+
+    [pwb] make_dist [options]
+
+.. note:: Requires Python 3.6+
+.. versionadded:: 7.3
+"""
+#
+# (C) Pywikibot team, 2022
+#
+# Distributed under the terms of the MIT license.
+#
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+from pywikibot import __version__, error, info, input_yn, warning
+from pywikibot.backports import Tuple
+import setup
+
+
+def copy_files() -> None:
+    """Copy code entry point and i18n files to pywikibot.scripts folder.
+
+    pwb.py wrapper script is a console script entry point for the
+    site-package. pywikibot i18n files are used for some translations.
+    They are copied to the pywikibot scripts folder.
+    """
+    folder = Path().resolve()
+    info(f'directory is {folder}')
+
+    # copy script entry points to pywikibot\scripts
+    target = folder / 'pywikibot' / 'scripts'
+    filename = 'pwb.py'
+    info(f'copy script entry point {filename!r} to {target}... ',
+         newline=False)
+    shutil.copy(folder / filename, target / filename)
+    info('done')
+
+    target = target / 'i18n' / 'pywikibot'
+    info(f'copy i18n files to {target} ... ', newline=False)
+    target.parent.mkdir()
+    filename = '__init__.py'
+    shutil.copy(folder / 'scripts' / 'i18n' / filename,
+                target.parent / filename)
+    shutil.copytree(folder / 'scripts' / 'i18n' / 'pywikibot', target)
+    info('done')
+
+
+def cleanup() -> None:
+    """Remove all files which were copied to the pywikibot scripts folder."""
+    info('Remove copied files... ', newline=False)
+    folder = Path().resolve()
+    target = folder / 'pywikibot' / 'scripts' / 'i18n'
+    shutil.rmtree(target)
+    target = target.parent / 'pwb.py'
+    target.unlink()
+    info('done')
+
+
+def handle_args() -> Tuple[bool, bool]:
+    """Handle arguments and print documentation if requested.
+
+    Read arguments from `sys.argv` and adjust it passing `sdist` to
+    `setuptools.setup`.
+
+    :return: Return whether dist is to be installed locally or to be
+        uploaded
+    """
+    if '-help' in sys.argv:
+        info(__doc__)
+        info(setup.__doc__)
+        sys.exit()
+
+    local = '-local' in sys.argv
+    remote = '-remote' in sys.argv
+
+    if remote and 'dev' in __version__:
+        warning('Distribution must not be a developmental release to upload.')
+        remote = False
+
+    sys.argv = [sys.argv[0], 'sdist']
+    return local, remote
+
+
+def main() -> None:
+    """Script entry point."""
+    local, remote = handle_args()
+
+    copy_files()
+
+    try:
+        setup.main()  # create a new package
+    except SystemExit as e:
+        error(e)
+        return
+    finally:
+        cleanup()
+
+    if local:
+        subprocess.run('pip uninstall pywikibot -y')
+        subprocess.run('pip install --no-index --find-links=dist pywikibot')
+
+    if remote and input_yn(
+            '<<lightblue>>Upload dist to pypi', automatic_quit=False):
+        subprocess.run('twine upload dist/*')
+
+
+if __name__ == '__main__':
+    main()
