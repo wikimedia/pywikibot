cf54b470e0cae0b227af7df293a33596ab857d58
xqt <info@gno.de>
2022-06-20 12:05:53 +0200

    [tests] Update TestMementoWebCite
    Bug: T193924
    Change-Id: I2a4403ad3983da1ef90b0ce6f1c6a29fb10379e5

--- tests/memento_tests.py
+++ tests/memento_tests.py
@@ -37,20 +37,19 @@ class MementoTestCase(TestCase):
         return result
 
 
-class TestMementoWebCite(MementoTestCase):
+class TestMementoArchive(MementoTestCase):
 
     """New WebCite Memento tests."""
 
-    timegate_uri = 'http://timetravel.mementoweb.org/webcite/timegate/'
-    hostname = ('http://timetravel.mementoweb.org/webcite/'
-                'timemap/json/http://google.com')
+    timegate_uri = 'http://timetravel.mementoweb.org/timegate/'
+    hostname = timegate_uri.replace('gate/', 'map/json/http://google.com')
 
     def test_newest(self):
-        """Test WebCite for newest https://google.com."""
+        """Test Archive for newest https://google.com."""
         archivedversion = self._get_archive_url('https://google.com')
         parsed = urlparse(archivedversion)
         self.assertIn(parsed.scheme, ['http', 'https'])
-        self.assertEqual(parsed.netloc, 'www.webcitation.org')
+        self.assertEqual(parsed.netloc, 'archive.ph')
 
 
 class TestMementoDefault(MementoTestCase):
