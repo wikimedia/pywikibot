f8699f64862033553e0c7564e852fff231818617
xqt <info@gno.de>
2022-06-21 14:12:43 +0200

    [IMPR] Add an empty id to wikibase simulate response.
    This is necessary to solve a KeyError within WikibaseEntity.editEntity()
    when simulating this method.
    
    Change-Id: I737d9e0c3bca1f427af7141b1251ec60872ef42e

--- pywikibot/data/api/_requests.py
+++ pywikibot/data/api/_requests.py
@@ -532,7 +532,7 @@ class Request(MutableMapping):
                 action: {'result': 'Success', 'nochange': ''},
 
                 # wikibase results
-                'entity': {'lastrevid': -1},
+                'entity': {'lastrevid': -1, 'id': '-1'},
                 'pageinfo': {'lastrevid': -1},
                 'reference': {'hash': -1},
             }
