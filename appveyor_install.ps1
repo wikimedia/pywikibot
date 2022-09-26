726f8468811d87418fbb590c6270fc9bffb44aff
xqt <info@gno.de>
2022-09-26 08:46:31 +0200

    [tests] Drop Python 3.5 tests on appveyor
    - remove Python 3.5 tests
    - add Python 3.6.0 test
    - remove one Python 3.10 test to decrease test time
    - remove pip 21 installation
    - change appveyor_install.ps1 to install 3.6.0
    
    Bug:T301908
    Change-Id: Icf3f4271c98210d494c4d74ea2565902f13a4bec

--- appveyor_install.ps1
+++ appveyor_install.ps1
@@ -60,7 +60,7 @@ function ParsePythonVersion ($python_version) {
 function DownloadPython ($python_version, $platform_suffix) {
     $major, $minor, $micro, $prerelease = ParsePythonVersion $python_version
 
-    # Only Python 3.5.3+ is supported
+    # Only Python 3.6.0+ is supported
     $dir = "$major.$minor.$micro"
     $ext = "exe"
     if ($platform_suffix) {
@@ -115,7 +115,6 @@ function InstallPythonEXE ($exepath, $python_home, $install_log, $unexepath) {
     RunCommand $unexepath $uninstall_args
     $install_args = "/log $install_log /quiet InstallAllUsers=1 TargetDir=$python_home\"
     RunCommand $exepath $install_args
-    RunCommand "python" '-m pip install -U "pip<21"'
 }
 
 
@@ -126,7 +125,7 @@ function RunCommand ($command, $command_args) {
 
 
 function main () {
-    if ($env:PYTHON_VERSION -eq "3.5.3") {
+    if ($env:PYTHON_VERSION -eq "3.6.0") {
         InstallPython $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     }
 }
