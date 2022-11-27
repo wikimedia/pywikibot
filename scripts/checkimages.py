035d7aa6e35fe4eb1192a7b4ea5ac27d0871c923
Matěj Suchánek <matejsuchanek97@gmail.com>
2022-11-27 19:56:06 +0000

    Retrieve only one match
    Change-Id: I2e35bcc2e690d3fc0640dcf5e37928ef9d26087a

--- scripts/checkimages.py
+++ scripts/checkimages.py
@@ -610,10 +610,10 @@ class CheckImagesBot:
     def upload_bot_change_function(report_page_text, upload_bot_array) -> str:
         """Detect the user that has uploaded the file through upload bot."""
         regex = upload_bot_array[1]
-        results = re.findall(regex, report_page_text)
+        result = re.search(regex, report_page_text)
 
-        if results:
-            return results[0]
+        if result:
+            return result.group()
 
         # we can't find the user, report the problem to the bot
         return upload_bot_array[0]
