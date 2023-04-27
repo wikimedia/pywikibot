32a94aa64572fe121cb706dbb00634e1d1ec628d
Stang <stang@toolforge.org>
2023-04-27 10:57:21 -0400

    Update closed_wikis: cnwikimedia closed
    Bug: T274083
    Change-Id: Ida625ebf3365cf9acc8c149bb03f1d935bf99aa9

--- pywikibot/families/wikimediachapter_family.py
+++ pywikibot/families/wikimediachapter_family.py
@@ -16,10 +16,10 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
         'et': 'ee'
     }
 
-    closed_wikis = ['nz', 'pa-us', ]
+    closed_wikis = ['cn', 'nz', 'pa-us', ]
 
     codes = [
-        'am', 'ar', 'az', 'bd', 'be', 'br', 'ca', 'cn', 'co', 'dk', 'ec', 'ee',
+        'am', 'ar', 'az', 'bd', 'be', 'br', 'ca', 'co', 'dk', 'ec', 'ee',
         'fi', 'ge', 'gr', 'hi', 'id', 'id-internal', 'il', 'mai', 'mk', 'mx',
         'ng', 'nl', 'no', 'nyc', 'pl', 'pt', 'punjabi', 'romd', 'rs', 'ru',
         'se', 'tr', 'ua', 'uk', 've', 'wb',
