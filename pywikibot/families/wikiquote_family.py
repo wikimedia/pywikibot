0ac961bb632559ded44cbdcc96979db44cdc02e6
Meno25 <meno25mail@gmail.com>
2023-06-29 01:59:19 +0000

    [L10N] Update languages_by_size
    Change-Id: I2f7a8e728ab9641360229143329e8dcc3f6169fd

--- pywikibot/families/wikiquote_family.py
+++ pywikibot/families/wikiquote_family.py
@@ -31,7 +31,7 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
         'es', 'tr', 'eo', 'sk', 'bs', 'az', 'ca', 'fi', 'sr', 'lt', 'zh', 'sl',
         'ar', 'su', 'id', 'bg', 'hy', 'hr', 'el', 'nn', 'sv', 'li', 'hu', 'ko',
         'nl', 'ja', 'sah', 'la', 'ta', 'hi', 'ig', 'gl', 'gu', 'ur', 'guw',
-        'be', 'te', 'vi', 'as', 'tl', 'cy', 'bn', 'no', 'sq', 'ml', 'kn', 'ro',
+        'be', 'te', 'as', 'vi', 'tl', 'cy', 'bn', 'no', 'sq', 'ml', 'kn', 'ro',
         'eu', 'ku', 'uz', 'ka', 'da', 'sa', 'is', 'bcl', 'th', 'br', 'mr',
         'af', 'ky',
     ]
