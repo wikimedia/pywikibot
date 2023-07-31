e6dfb8384d64fc6915f00688d15f44ae22360c9d
xqt <info@gno.de>
2023-07-31 10:49:52 +0200

    [sphinx] use Sphinx >= 7.1.0 and furo >= 2023.7.26
    Change-Id: I6df5d09f875dde883733fb7b188e70ab5c3131c3

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ import pywikibot  # noqa: E402
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '6.2.1'
+needs_sphinx = '7.1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
