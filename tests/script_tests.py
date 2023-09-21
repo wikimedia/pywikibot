b1d58e964f06c3ac5d7bab1b29422dad98ccfbcf
Xqt <info@gno.de>
2023-09-21 18:19:57 +0000

    Revert "[tests] Speedup script_tests"
    This reverts commit 39989ca2fed731c38f321d18fa79f92ce34cc6f2.
    
    Reason for revert: failing collector
    
    Change-Id: I9cd39ed98b0ca25c65f74754886eacad422801d2

--- tests/script_tests.py
+++ tests/script_tests.py
@@ -306,15 +306,13 @@ class ScriptTestMeta(MetaTestCaseClass):
                            .format(script_name, arguments))
 
             if script_name in dct['_expected_failures']:
-                if name == 'TestScriptGenerator':
-                    continue  # do not test it
                 dct[test_name] = unittest.expectedFailure(dct[test_name])
             elif script_name in dct['_allowed_failures']:
                 dct[test_name] = unittest.skip(
                     '{} is in _allowed_failures set'
                     .format(script_name))(dct[test_name])
             elif script_name in failed_dep_script_set \
-                    and name == 'TestScriptSimulate':
+                    and arguments == '-simulate':
                 dct[test_name] = unittest.skip(
                     '{} has dependencies; skipping'
                     .format(script_name))(dct[test_name])
