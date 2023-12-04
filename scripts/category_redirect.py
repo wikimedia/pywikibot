589b554330d3414aff7ea3e55c59d5111aa23487
xqt <info@gno.de>
2023-12-04 08:36:48 +0100

    [doc] Fix typo in category_redirect.py
    Bug: T351951
    Change-Id: I78c5cc5c0ec888b5c6d3cc6dbbc01eddcb9ac227

--- scripts/category_redirect.py
+++ scripts/category_redirect.py
@@ -14,8 +14,8 @@ The following parameters are supported:
 -always           If used, the bot won't ask if it should add the specified
                   text
 
--delay:#          Set an amount of days. If the category is edited more recenty
-                  than given days, ignore it. Default is 7.
+-delay:#          Set an amount of days. If the category is edited more
+                  recently than given days, ignore it. Default is 7.
 
 -tiny             Only loops over Category:Non-empty_category_redirects and
                   moves all images, pages and categories in redirect categories
@@ -30,7 +30,7 @@ Usage:
    can be set within a settings file which is scripts.ini by default.
 """
 #
-# (C) Pywikibot team, 2008-2022
+# (C) Pywikibot team, 2008-2023
 #
 # Distributed under the terms of the MIT license.
 #
