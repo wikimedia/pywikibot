669ae5f858703f7e8a2bbec321a0c6f8009bfdb1
xqt <info@gno.de>
2023-12-28 18:50:56 +0100

    [IMPR] Add config.base_dir to scripts search path
    Bug: T324287
    Change-Id: I0d6a2d86daadee499df863ff1886ac799a9c03db

--- pywikibot/scripts/wrapper.py
+++ pywikibot/scripts/wrapper.py
@@ -44,7 +44,6 @@ for tests to set the default site (see :phab:`T216825`)::
 #
 from __future__ import annotations
 
-#
 import os
 import sys
 import types
@@ -379,6 +378,8 @@ def find_filename(filename):
 
     .. versionchanged:: 7.0
        Search users_scripts_paths in config.base_dir
+    .. versionchanged:: 9.0
+       Add config.base_dir to search path
     """
     from pywikibot import config
     path_list = []  # paths to find misspellings
@@ -394,14 +395,14 @@ def find_filename(filename):
             path_list.append(testpath.parent)
         return None
 
-    user_script_paths = []
+    user_script_paths = ['']
     if config.user_script_paths:  # pragma: no cover
         if isinstance(config.user_script_paths, list):
-            user_script_paths = config.user_script_paths
+            user_script_paths += config.user_script_paths
         else:
             warn("'user_script_paths' must be a list,\n"
-                 'found: {}. Ignoring this setting.'
-                 .format(type(config.user_script_paths)))
+                 f'found: {type(config.user_script_paths).__name__}.'
+                 ' Ignoring this setting.')
 
     found = test_paths(user_script_paths, Path(config.base_dir))
     if found:  # pragma: no cover
