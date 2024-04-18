9fc1b8a3591815ecb59ee9213fdd945ec28598cd
Xqt <info@gno.de>
2024-04-18 04:38:39 +0000

    [tests] alllinks fails for en-wiki too
    Bug: T359427
    Change-Id: I7989075774cfd50c8bc7167551ddb8890a87e264
    Signed-off-by: Xqt <info@gno.de>

--- tests/site_generators_tests.py
+++ tests/site_generators_tests.py
@@ -337,7 +337,7 @@ class TestSiteGenerators(DefaultSiteTestCase):
     def test_all_links(self):
         """Test the site.alllinks() method."""
         mysite = self.get_site()
-        if mysite.sitename == 'wikipedia:de':
+        if mysite.sitename in ('wikipedia:de', 'wikipedia:en'):
             self.skipTest(f'skipping test on {mysite} due to T359427')
         fwd = list(mysite.alllinks(total=10))
         uniq = list(mysite.alllinks(total=10, unique=True))
