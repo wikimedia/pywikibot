2153e5dfd1fa1554eb2eec89b53219e688da2175
xqt <info@gno.de>
2024-04-28 15:16:36 +0200

    [doc] update sphinx requirements
    Change-Id: Idb580cadd038c417d6c4ca20089935470f41c3aa

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ import pywikibot  # noqa: E402
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '7.2.6'
+needs_sphinx = '7.3.7'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
