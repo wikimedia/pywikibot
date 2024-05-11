47c5b8d20889a4ec2b1c091755a3c1ce769782d1
xqt <info@gno.de>
2024-05-11 16:34:22 +0200

    [doc] update maintenance documentation
    Change-Id: I66c4aa4cc4f7075ca0d764c254d18e0cb6ae4679

--- scripts/maintenance/__init__.py
+++ scripts/maintenance/__init__.py
@@ -1,5 +1,14 @@
 """Maintenance scripts.
 
-.. versionchanged:: 8.0
-   sorting_order script was removed.
+.. versionremoved:: 3.0.20190430
+   ``diff_checker`` script was removed (:phab:`T221462`).
+.. versionremoved:: 6.1
+   ``compat2core`` script was removed.
+.. versionremoved:: 7.3
+   ``update_linktrails`` script (:phab:`T89451`) and ``update_script``
+   script were removed.
+.. versionremoved:: 8.0
+   ``sorting_order`` script was removed (:phab:`T325426`).
+.. versionremoved:: 9.0
+   ``wikimedia_sites`` script was removed (:phab:`T78396`).
 """
