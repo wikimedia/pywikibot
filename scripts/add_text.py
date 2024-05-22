c1a616b1cff6efc3c8b0bb442fd5b440edcd8434
xqt <info@gno.de>
2024-05-22 12:40:57 +0200

    [doc] Fix some sphinx errors
    Change-Id: I4f1420c5a08e51288be888ce07e138fe3b90c9bf

--- scripts/add_text.py
+++ scripts/add_text.py
@@ -38,25 +38,25 @@ Furthermore, the following can be used to specify which pages to process...
 Examples
 --------
 
-1. Append 'hello world' to the bottom of the sandbox:
+Append 'hello world' to the bottom of the sandbox:
 
     python pwb.py add_text -page:Wikipedia:Sandbox \
--summary:"Bot: pywikibot practice" -text:"hello world"
+    -summary:"Bot: pywikibot practice" -text:"hello world"
 
-2. Add a template to the top of the pages with 'category:catname':
+Add a template to the top of the pages with 'category:catname':
 
     python pwb.py add_text -cat:catname -summary:"Bot: Adding a template" \
--text:"{{Something}}" -except:"\{\{([Tt]emplate:|)[Ss]omething" -up
+    -text:"{{Something}}" -except:"\{\{([Tt]emplate:|)[Ss]omething" -up
 
-3. Command used on it.wikipedia to put the template in the page without any
-   category:
+Command used on it.wikipedia to put the template in the page without any
+category:
 
     python pwb.py add_text -except:"\{\{([Tt]emplate:|)[Cc]ategorizzare" \
--text:"{{Categorizzare}}" -excepturl:"class='catlinks'>" -uncat \
--summary:"Bot: Aggiungo template Categorizzare"
+    -text:"{{Categorizzare}}" -excepturl:"class='catlinks'>" -uncat \
+    -summary:"Bot: Aggiungo template Categorizzare"
 """
 #
-# (C) Pywikibot team, 2007-2023
+# (C) Pywikibot team, 2007-2024
 #
 # Distributed under the terms of the MIT license.
 #
