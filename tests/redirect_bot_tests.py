677b565cd8fd2445f48ac0135fcff951f9bb0a49
xqt <info@gno.de>
2025-06-10 12:19:53 +0200

    IMPR: Use wikidata items broken redirect templates
    - Use wikidata items speedy deletion templates if sdtemplate option is
      not given
    - remove get_sd_template method and move statements into sdtemplate property
    - update redirect_bot_tests
    
    Bug: T396447
    Change-Id: I0f8d3ca156ff03585e0aa627048dc537075bc4da

--- tests/redirect_bot_tests.py
+++ tests/redirect_bot_tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Tests for the redirect.py script."""
 #
-# (C) Pywikibot team, 2017-2024
+# (C) Pywikibot team, 2017-2025
 #
 # Distributed under the terms of the MIT license.
 #
@@ -12,7 +12,7 @@ from contextlib import suppress
 from unittest.mock import Mock, patch
 
 import pywikibot
-from pywikibot import Page, i18n
+from pywikibot import Page
 from scripts.redirect import RedirectRobot
 from tests.aspects import DefaultSiteTestCase
 
@@ -43,20 +43,23 @@ class TestGetSDTemplateNoSysop(DefaultSiteTestCase):
         bot = RedirectTestRobot('broken', **options)
         self.assertEqual(bot.sdtemplate, '{{t}}')
 
-    @patch.object(i18n, 'twhas_key', new=Mock(return_value=True))
-    @patch.object(i18n, 'twtranslate', new=Mock(return_value='{{sd_title}}'))
     def test_with_delete_and_i18n_sd(self) -> None:
-        """Test with delete and i18n template."""
-        bot = RedirectTestRobot('broken', delete=True)
-        self.assertEqual(bot.sdtemplate, '{{sd_title}}')
+        """Test with delete and wikibase template."""
+        with patch.object(
+            pywikibot.site.APISite, 'page_from_repository',
+            new=Mock(return_value=pywikibot.Page(self.site, 'sd_title'))
+        ):
+            bot = RedirectTestRobot('broken', delete=True)
+            self.assertEqual(bot.sdtemplate, '{{Sd title}}')
 
-    @patch.object(i18n, 'twhas_key', new=Mock(return_value=False))
     def test_with_delete_no_sd_no_i18n(self) -> None:
-        """Test with delete and no i18n template."""
-        bot = RedirectTestRobot('broken', delete=True)
-        with patch.object(pywikibot, 'warning') as w:
-            self.assertIsNone(bot.sdtemplate)
-        w.assert_called_with('No speedy deletion template available.')
+        """Test with delete and no wikibase template."""
+        with patch.object(pywikibot.site.APISite, 'page_from_repository',
+                          new=Mock(return_value=None)):
+            bot = RedirectTestRobot('broken', delete=True)
+            with patch.object(pywikibot, 'warning') as w:
+                self.assertEqual(bot.sdtemplate, '')
+            w.assert_called_with('No speedy deletion template available.')
 
     def test_with_delete_and_non_existing_sdtemplate(self) -> None:
         """Test with delete and non-existing sdtemplate."""
@@ -64,7 +67,7 @@ class TestGetSDTemplateNoSysop(DefaultSiteTestCase):
         bot = RedirectTestRobot('broken', **options)
         with patch.object(Page, 'exists', new=Mock(return_value=False)), \
                 patch.object(pywikibot, 'warning') as w:
-            self.assertIsNone(bot.sdtemplate, None)
+            self.assertEqual(bot.sdtemplate, '')
         w.assert_called_with('No speedy deletion template "n" available.')
 
 
