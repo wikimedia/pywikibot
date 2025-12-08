54f96359353052a5af83cba56d12bdcb6f632a2e
Xqt <info@gno.de>
2025-12-08 15:15:06 +0000

    doc: Update Sphinx
    Change-Id: I411c3b81743dc61bea76b05be94dcf4c4c825361
    Signed-off-by: Xqt <info@gno.de>

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ import pywikibot  # noqa: E402
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '8.2.3'
+needs_sphinx = '9.0.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
