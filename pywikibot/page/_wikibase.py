362279dfb90abd3c455fc1c139a32dfd57ac6421
Marv Osuolale <mahveotm@gmail.com>
2026-08-17 18:20:21 +0100

    wikibase: Reuse repository and page properties
    Resolve the data repository once while creating an item from a page.
    
    Reuse cached page properties when selecting an existing item ID.
    
    Change-Id: Ibfe17f898f1ccd5c7c93b9998df46c950400cdb8

--- pywikibot/page/_wikibase.py
+++ pywikibot/page/_wikibase.py
@@ -1073,18 +1073,19 @@ class ItemPage(WikibasePage):
         """
         if hasattr(page, '_item'):
             return page._item
-        if not page.site.has_data_repository:
+        repo = page.site.data_repository()
+        if repo is None:
             raise WikiBaseError(f'{page.site} has no data repository')
         if not lazy_load and not page.exists():
             raise NoPageError(page)
 
-        repo = page.site.data_repository()
-        if hasattr(page,
-                   '_pageprops') and page.properties().get('wikibase_item'):
+        if hasattr(page, '_pageprops'):
             # If we have already fetched the pageprops for something else,
             # we already have the id, so use it
-            page._item = cls(repo, page.properties().get('wikibase_item'))
-            return page._item
+            item_id = page.properties().get('wikibase_item')
+            if item_id:
+                page._item = cls(repo, item_id)
+                return page._item
         i = cls(repo)
         # clear id, and temporarily store data needed to lazy loading the item
         del i.id
